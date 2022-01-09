54562eec8dee6705c0317b37a9892ec3af81e8dc
Orion Junkins <orionjunkins@gmail.com>
2022-01-08 16:10:41 -0800

    Docstrings added/improved

--- src/dataset.py
+++ src/dataset.py
@@ -18,7 +18,13 @@ class Dataset:
         self._process_level_url(level_url)
 
 
-    def _process_level_url(self, level_url):
+    def _process_level_url(self, level_url) -> None:
+        """
+        Fetch data from the given level url and perform basic processing.
+
+        Args:
+            level_url (string): exact url linking to the desired CSV file
+        """
         self.level_data = pd.read_csv(level_url, sep='\t', comment='#') 
 
         cols_to_drop = [col for col in self.level_data.columns if 'cd' in col]
@@ -47,16 +53,31 @@ class Dataset:
 
 
     def _process_all_weather_urls(self, weather_urls):
+        """ 
+        Fetch and process data from every given weather url in the list
+
+        Args:
+            weather_urls (list): List of exact urls linking to CSV files for the target weather stations.
+        """
         for url in weather_urls:
             self._process_weather_url(url)
 
     def _process_weather_url(self, url):
+        """
+        Fetch data from the given url, proccess it and add it to the list of weather datasets.
+
+        Args:
+            url (string): exact url linking to the target CSV
+        """
         weather_data = pd.read_csv(url, comment='#')  
 
     
-def yesterday():
-    # Helper function for data retrieval
-    # Returnss yesterdays date in the format "%Y-%m-%d"
+def yesterday() -> str:
+    """
+    Helper function for data retrieval
+    Returns:
+        yesterday (string): yesterdays date in the format "%Y-%m-%d"
+    """
     yesterday = datetime.today()  - timedelta(days=1)
     yesterday = yesterday.strftime("%Y-%m-%d")
     return yesterday
