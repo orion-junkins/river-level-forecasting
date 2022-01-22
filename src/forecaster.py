0073d658d97e057640860d20a9c1a64f0a272a45
orion-junkins <orionjunkins@gmail.com>
2022-01-22 12:35:43 -0800

    Forecaster functionality improved to simplify external API

--- src/forecaster.py
+++ src/forecaster.py
@@ -6,34 +6,41 @@ from dataset import *
 class Forecaster:
     """
     """
-    def __init__(self, weather_locations, gauge_url, model) -> None:
+    def __init__(self, weather_locations, gauge_url, model_builder) -> None:
         """
         
         """
         self.historical_data = Dataset(weather_locations, gauge_url)
-        self.model = model.fit(self.data.X_train_shaped, self.data.y_train, 
+        self.model = model_builder(self.historical_data.input_shape)
+        
+        self.current_data = self.historical_data.X_test_shaped #InferenceSet(weather_locations)
+        self.forecasted_levels = []
+    def fit(self):
+        self.model.fit(self.historical_data.X_train_shaped, self.historical_data.y_train, 
                         epochs = 20, 
                         batch_size = 10, 
                         shuffle = True)
-    
 
-    @property
-    def forecast(self):
+    def forecast_for(self, index):
         """
+        Take in the index for which a forecast is desired
         """
-        self.model.run_inference(self.x_in)
+        x_in = self.current_data[index:index+1]
+        y_pred = np.array(self.model.predict(x_in))
+        target_scaler = self.historical_data.target_scaler
+        y_pred = target_scaler.inverse_transform(y_pred)
+        return y_pred[0][0]  
 
 
-    def forecast_for(self, timestamp) -> str:
+    def forecast_all(self):
         """
         """
-        expected_level = self.forecast[timestamp]
-        
-        return "Expected level for " + timestamp + " is " + expected_level
+        for i in range(len(self.current_data)):
+            expected_level = self.forecast_for(i)
+            self.forecasted_levels.append(expected_level)
 
 
     def update_input_data(self, input_data) -> None:
         """
         """
         self.input_data = input_data
-        
