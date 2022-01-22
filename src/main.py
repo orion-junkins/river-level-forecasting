5e339b52ac106ec4dba7cc688b018f58eddbe1b0
orion-junkins <orionjunkins@gmail.com>
2022-01-22 12:34:50 -0800

    Main updated to use updated Forecaster features

--- src/main.py
+++ src/main.py
@@ -42,16 +42,18 @@ mck_vida_url = 'https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_n
 # Fetch weather & SWE data for Mckenzie basin. Atuomatically retrieves all data up to present.
 mck1_weather_url ='https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/619:OR:SNTL%7Cid=%22%22%7Cname/POR_BEGIN,POR_END/WTEQ::value,PREC::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value'
 
-mck2_weather_url = 'https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultiTimeSeriesGroupByStationReport/daily/start_of_period/719:OR:SNTL%7Cid=%22%22%7Cname/1980-11-03,2022-01-08/WTEQ::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value?fitToScreen=false'
-
-ds = Dataset([mck1_weather_url, mck2_weather_url], mck_vida_url)
-
-model = build_conv_model(ds.input_shape)
+mck2_weather_url = 'https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customMultiTimeSeriesGroupByStationReport/daily/start_of_period/719:OR:SNTL%7Cid=%22%22%7Cname/1980-11-03,2022-01-18/WTEQ::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value?fitToScreen=false'
 
 # %%
-frcstr = Forecaster([mck1_weather_url, mck2_weather_url], mck_vida_url, model)
+frcstr = Forecaster([mck1_weather_url, mck2_weather_url], mck_vida_url, build_conv_model)
 
+frcstr_backup = frcstr
+#%%
+frcstr.fit()
 
+#%%
+frcstr.forecast_all()
+print(frcstr.forecasted_levels)
 # %%
 def plot_predictions(y_pred, y_true, start_index=0, end_index=-1):
     plt.plot(y_pred[start_index:end_index], label='y_pred')
