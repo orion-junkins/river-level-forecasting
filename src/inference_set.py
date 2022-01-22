3a2f09050f9caec7b45c77b06bd22ab784c8b178
orion-junkins <orionjunkins@gmail.com>
2022-01-22 12:42:37 -0800

    Import typos corrected

--- src/inference_set.py
+++ src/inference_set.py
@@ -1,7 +1,7 @@
-from data_fetching_utilities.open_weather import *
-from data_fetching_utilities.nwis import *
+from data_fetching_utilities.weather import *
+from data_fetching_utilities.level import *
 
 class InferenceSet:
     def __init__(self, locations, level_url) -> None:
         self.forecasts = fetch_forecasts(locations)
-        self.level = get_hourly("14162500", start='1990-01-01')
+        self.level = get_historical_level("14162500", start='1990-01-01')
