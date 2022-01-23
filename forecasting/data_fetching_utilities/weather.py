44c77d2e15209052c107ed257c56413fe18cb4e3
orion-junkins <orionjunkins@gmail.com>
2022-01-23 09:28:55 -0800

    Recent weather fetching working (basic functionality)

--- forecasting/data_fetching_utilities/weather.py
+++ forecasting/data_fetching_utilities/weather.py
@@ -3,19 +3,19 @@ import pandas as pd
 import json
 import urllib.request, json 
 from datetime import datetime
+import regex as re
 
 from forecasting.data_fetching_utilities.open_weather_api_keys import api_key
 
 # TODO investigate if any of these could be useful    
 OPEN_WEATHER_DEFAULT_DROP_COLS = ['dt', 'dt_iso', 'timezone', 'city_name', 'lat', 'lon', 'visibility', 'sea_level', 'grnd_level', 'wind_gust', 'weather_description', 'weather_icon', 'clouds_all', 'weather_id', 'weather_main', 'rain_3h', 'snow_3h']
+OPEN_WEATHER_COLUMNS = []
 
-
-def get_forecast(lat, lon, start=None, excludes="current,minutely,hourly,alerts", api_key=api_key):
+def get_forecast(lat, lon, excludes="current,minutely,hourly,alerts", api_key=api_key):
     request_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={excludes}&appid={api_key}"
     
     with urllib.request.urlopen(request_url) as url:
         data = json.loads(url.read().decode())
-        print(data)
     df = pd.json_normalize(data, record_path =['daily'])
     return df
 
@@ -27,7 +27,7 @@ def get_forecasts(locations):
     return forecasts
 
 
-def get_single_historical(path, drop_cols=OPEN_WEATHER_DEFAULT_DROP_COLS):
+def get_single_loc_historical(path, drop_cols=OPEN_WEATHER_DEFAULT_DROP_COLS):
     df_weather = pd.read_csv(path)
     df_weather['datetime'] = list(map(datetime.fromtimestamp, df_weather['dt'])) 
     df_weather.set_index('datetime', inplace=True)
@@ -46,7 +46,28 @@ def get_single_historical(path, drop_cols=OPEN_WEATHER_DEFAULT_DROP_COLS):
 def get_all_historical_weather(paths, drop_cols=OPEN_WEATHER_DEFAULT_DROP_COLS):
     dfs_historical = []
     for path in paths:
-        df_historical = get_single_historical(path, drop_cols=drop_cols)
+        df_historical = get_single_loc_historical(path, drop_cols=drop_cols)
         dfs_historical.append(df_historical)
     
     return dfs_historical
+
+def get_single_loc_recent(lat, lon, start, end, api_key=api_key):
+    request_url = f"http://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&units=imperial&type=hour&start={start}&end={end}&appid={api_key}"
+    print(request_url)
+    with urllib.request.urlopen(request_url) as url:
+        data = json.loads(url.read().decode())
+    df_recent = pd.json_normalize(data, record_path =['list'])
+    df_recent['datetime'] = list(map(datetime.fromtimestamp, df_recent['dt'])) 
+    df_recent.set_index('datetime', inplace=True)
+    df_recent = df_recent.rename(columns=lambda x: re.sub('main.','',x))
+    return df_recent
+
+
+def get_all_recent_weather(weather_locs, start):
+    dfs_recent = []
+    for loc in weather_locs:
+        lat = loc[0]
+        lon = loc[1]
+        df_recent = get_single_loc_recent(lat, lon, start)
+        dfs_recent.append(df_recent)
+    return dfs_recent
