9bf3fbf9a61d01c6325b0a4fb7f985d4ee875b94
orion-junkins <orionjunkins@gmail.com>
2022-01-22 17:07:24 -0800

    Filestructure re-worked to clarify package structure

--- forecasting/inference_set.py
+++ forecasting/inference_set.py
@@ -2,6 +2,6 @@ from data_fetching_utilities.weather import *
 from data_fetching_utilities.level import *
 
 class InferenceSet:
-    def __init__(self, locations, level_url) -> None:
-        self.forecasts = fetch_forecasts(locations)
+    def __init__(self, forecast_site, historical_weather_paths) -> None:
+        self.forecast_site = forecast_site
         self.level = get_historical_level("14162500", start='1990-01-01')
--- src/data_fetching_utilities/open_weather_api_keys.py
+++
@@ -1,2 +0,0 @@
-# API key for Open Weather API retrievals
-api_key = "3857fc663657cfade7eccda620b36a35"
