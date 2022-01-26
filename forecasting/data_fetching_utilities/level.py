083830ad224adfb0f18fb6ae5568eade51238d80
orion-junkins <orionjunkins@gmail.com>
2022-01-25 21:08:01 -0800

    Datetime indexing errors corrected

--- forecasting/data_fetching_utilities/level.py
+++ forecasting/data_fetching_utilities/level.py
@@ -1,7 +1,7 @@
 import dataretrieval.nwis as nwis
 import pandas as pd
 
-def get_historical_level(gauge_id, start="1900-01-01", end=None, parameterCd='00060', drop_cols=["00060_cd", "site_no"], rename_dict={"00060":"level"} ):
+def get_historical_level(gauge_id, start="2019-01-01", end=None, parameterCd='00060', drop_cols=["00060_cd", "site_no"], rename_dict={"00060":"level"} ):
     """
     Fetch level data for the given gauge ID. Fetches instant values from start to end.
     Drops and renames columns according to given args.
@@ -15,22 +15,22 @@ def get_historical_level(gauge_id, start="1900-01-01", end=None, parameterCd='00
         rename_dict (dict, optional): Dictionary of default:new defining column renamings. Defaults to {"00060":"level"}.
 
     Returns:
-        df_level (Pandas dataframe): Formatted dataframe of fetched data
+        df (Pandas dataframe): Formatted dataframe of fetched data
     """
     # Fetch level data
-    df_level = nwis.get_record(sites=gauge_id, service='iv', start=start, end=end, parameterCd=parameterCd)
+    df = nwis.get_record(sites=gauge_id, service='iv', start=start, end=end, parameterCd=parameterCd)
 
     # Filter out any columns that are present in the drop_cols list
-    drop_cols = list(filter(lambda x: x in df_level.columns, drop_cols))
-    df_level.drop(columns=drop_cols, inplace=True)
+    drop_cols = list(filter(lambda x: x in df.columns, drop_cols))
+    df.drop(columns=drop_cols, inplace=True)
 
     # Rename columns as specified
-    df_level.rename(columns=rename_dict, inplace=True)
+    df.rename(columns=rename_dict, inplace=True)
     
-    # Convert index to datetime objects
-    df_level.index = pd.to_datetime(df_level.index)
-
-    df_level.index = df_level.index.tz_convert(None)
+    # Convert index to datetime objects TODO: remove double conversion
+    df.index = pd.to_datetime(df.index, utc=True)
+    df.index = pd.DatetimeIndex(df.index)
+    df.index = df.index.tz_convert(None)
 
     # Return the formatted dataframe
-    return df_level
+    return df
