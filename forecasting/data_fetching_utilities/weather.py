30c2fe04bf3252a21b9fceb94fd09b0d2cb8d619
orion-junkins <orionjunkins@gmail.com>
2022-01-25 16:37:15 -0800

    Weather fetching wrappers updated to produce consistent df cols

--- forecasting/data_fetching_utilities/weather.py
+++ forecasting/data_fetching_utilities/weather.py
@@ -7,52 +7,46 @@ import regex as re
 
 from forecasting.data_fetching_utilities.open_weather_api_keys import api_key
 
-# TODO investigate if any of these could be useful    
-OPEN_WEATHER_DEFAULT_DROP_COLS = ['dt', 'dt_iso', 'timezone', 'city_name', 'lat', 'lon', 'visibility', 'sea_level', 'grnd_level', 'wind_gust', 'weather_description', 'weather_icon', 'clouds_all', 'weather_id', 'weather_main', 'rain_3h', 'snow_3h']
-OPEN_WEATHER_COLUMNS = []
+# TODO explore other features beyond this set
+DEFAULT_WEATHER_COLS = ['temp','pressure', 'humidity', 'wind_speed', 'wind_deg', 'rain_1h', 'snow_1h']
 
 # Open Weather API wrapper functions
-def fetch_one_call(lat, lon, excludes="current,minutely,hourly,alerts", api_key=api_key):
-    request_url = f"https://api.openweathermap.org/data/2.5/onecall?lat={str(lat)}&lon={str(lon)}&exclude={excludes}&appid={api_key}"
-    df = fetch_to_dataframe(request_url, ['daily'])
-    return df
-
-
-def fetch_hourly_foreccast(lat, lon, api_key=api_key):
-    request_url = f"pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={api_key}"
+def fetch_hourly_forecast(lat, lon, api_key=api_key):
+    request_url = f"http://pro.openweathermap.org/data/2.5/forecast/hourly?lat={lat}&lon={lon}&appid={api_key}"
     df = fetch_to_dataframe(request_url, ['list'])
+    df = correct_columns(df)
     return df
 
 
-def fetch_recent_historical(lat, lon, start, end, api_key=api_key):
-    request_url = f"http://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&units=imperial&type=hour&start={start}&end={end}&appid={api_key}"
+def fetch_recent_historical(lat, lon, start, api_key=api_key):
+    request_url = f"http://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&units=imperial&type=hour&start={start}&appid={api_key}"
     df = fetch_to_dataframe(request_url, ['list'])
-    df = df.rename(columns=lambda x: re.sub('main.','',x))
+    df = correct_columns(df)
+    print(request_url)
     return df
 
 
-def load_single_loc_historical(path, drop_cols=OPEN_WEATHER_DEFAULT_DROP_COLS):
-    df_weather = pd.read_csv(path)
-    df_weather['datetime'] = list(map(datetime.fromtimestamp, df_weather['dt'])) 
-    df_weather.set_index('datetime', inplace=True)
+def load_single_loc_historical(path):
+    df = pd.read_csv(path)
+    df['datetime'] = list(map(datetime.fromtimestamp, df['dt'])) 
+    df.set_index('datetime', inplace=True)
 
-    # Filter out any columns that are present in the drop_cols list
-    drop_cols = list(filter(lambda x: x in df_weather.columns, drop_cols))
-    df_weather.drop(columns=drop_cols, inplace=True)
+    df = correct_columns(df)
 
     # Convert Nan precip values to 0.0
-    df_weather['rain_1h'].fillna(0.0, inplace=True)
-    df_weather['snow_1h'].fillna(0.0, inplace=True)
-    
-    return df_weather
+    df['rain_1h'].fillna(0.0, inplace=True)
+    df['snow_1h'].fillna(0.0, inplace=True)
+    return df
 
 
 
 # Wrappers to query multiple locations at once [] -> []
-def get_forecasted_weather(locations) -> list:
+def get_all_forecasted_weather(locations) -> list:
     forecasts = []
     for loc in locations:
-        forecasts.append(fetch_hourly_foreccast(loc[0], loc[1]))
+        lat = loc[0]
+        lon = loc[1]
+        forecasts.append(fetch_hourly_forecast(lat, lon))
     return forecasts
 
 
@@ -66,10 +60,10 @@ def get_all_recent_weather(weather_locs, start) -> list:
     return dfs_recent
 
     
-def get_all_historical_weather(paths, drop_cols=OPEN_WEATHER_DEFAULT_DROP_COLS) -> list:
+def get_all_historical_weather(paths) -> list:
     dfs_historical = []
     for path in paths:
-        df_historical = load_single_loc_historical(path, drop_cols=drop_cols)
+        df_historical = load_single_loc_historical(path)
         dfs_historical.append(df_historical)
     
     return dfs_historical
@@ -84,3 +78,23 @@ def fetch_to_dataframe(request_url, record_path):
     df['datetime'] = list(map(datetime.fromtimestamp, df['dt'])) 
     df.set_index('datetime', inplace=True)
     return df
+
+
+def correct_columns(df, target_cols=DEFAULT_WEATHER_COLS):
+    # Rename any cols with 'main.' prefix
+    df = df.rename(columns=lambda x: re.sub('main.','',x))
+
+    # Replace any remaining '.' chars in col names to '_'
+    df = df.rename(columns=lambda x: re.sub('\.','_',x))
+
+    # Filter out any columns that are present in the drop_cols list
+    drop_cols = list(filter(lambda x: x not in target_cols, df.columns))
+    df.drop(columns=drop_cols, inplace=True)
+
+    # Create columns of 0 for any missing (not observed) data
+    missing_cols = list(filter(lambda x: x not in df.columns, target_cols))
+    for col in missing_cols:
+        df[col] = 0
+
+    return df
+
