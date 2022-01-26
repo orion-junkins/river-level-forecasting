c01b85141b124f9c5d5dd46b2a662d3a213a5297
orion-junkins <orionjunkins@gmail.com>
2022-01-25 19:24:51 -0800

    Inference set improved to allow specific data windows to be fetched

--- forecasting/inference_set.py
+++ forecasting/inference_set.py
@@ -1,7 +1,28 @@
-from data_fetching_utilities.weather import *
-from data_fetching_utilities.level import *
+from forecasting.data_fetching_utilities.weather import *
+from forecasting.data_fetching_utilities.level import *
+from forecasting.forecast_site import ForecastSite
+from datetime import timedelta
 
 class InferenceSet:
-    def __init__(self, forecast_site, historical_weather_paths) -> None:
-        self.forecast_site = forecast_site
-        self.level = get_historical_level("14162500", start='1990-01-01')
+    def __init__(self, forecast_site) -> None:
+        self.data = forecast_site.all_inference_data
+
+    def data_for_window(self, start, end=None, window_size_hours=5):
+        if type(start) is not 'datetime.datetime':
+            start = datetime.fromisoformat(start)
+        print(type(start))
+        if end is None:
+            end = start + timedelta(hours=window_size_hours-1)
+
+        if start not in self.data.index:
+            print("ERROR: invalid start timestamp")
+            return None
+
+        if end not in self.data.index:
+            print("ERROR: invalid end timestamp")
+            return None
+
+        data_in_range = self.data.loc[start:end, :]
+        return data_in_range
+        
+
