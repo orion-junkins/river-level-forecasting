8e9653582f7f6780958850be1209b1f63bfb9739
orion-junkins <orionjunkins@gmail.com>
2022-02-03 19:32:37 -0800

    Dataset simplified and generalized with util functions extracted to separate file

--- forecasting/dataset.py
+++ forecasting/dataset.py
@@ -1,73 +1,39 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
 
+from forecasting.general_utilities.df_utils import *
 class Dataset:
 
-    def __init__(self, forecast_site) -> None:
+    def __init__(self, data_fetcher) -> None:
         self.scaler = MinMaxScaler()
         self.target_scaler = MinMaxScaler()
-        
-        self.data = self._pre_process(forecast_site.all_training_data)
-        self.X, self.y = self._build_windowed_X_y()
-        self.X_train, self.X_test, self.y_train, self.y_test = self._partition()
-        
-        # TODO Investigate best location in pipeline for reshaping
-        self.X_train_shaped = self.X_train.reshape(self.X_train.shape[0], self.X_train.shape[1], self.X_train.shape[2], 1)
-        self.X_test_shaped = self.X_test.reshape(self.X_test.shape[0], self.X_test.shape[1], self.X_test.shape[2], 1)
-        
-        self.input_shape = self.X_train_shaped[0].shape
-        
-        assert(self.data.isna().sum().sum() == 0)
+                
+        self.X_hisorical_processed, self.y_historical_processed = self._pre_process(data_fetcher.all_historical_data.copy())
+        self.X_hisorical_windowed, self.y_historical_windowed = get_all_windows(self.X_hisorical_processed, self.y_historical_processed)
+        self.X_train, self.X_test, self.y_train, self.y_test = partition(self.X_hisorical_windowed, self.y_historical_windowed)
 
+        #self.X_current, self.y_current = self._pre_process(data_fetcher.all_current_data.copy(), fit_scalers=False)
 
-    def _pre_process(self, training_data):
-        data = training_data.copy()
-        data = self._shift_targets(data)
-        data = self._scale(data)
-        return data
-
-    def _shift_targets(self, data):
-        data['target_level'] = np.nan
-        rows = data.shape[0]
-        for row_idx in range(0, rows-1):
-            data['target_level'][row_idx] = data['level'][row_idx+1]
-        data.drop(data.tail(1).index,inplace=True)
-        return data
-    
-
-    def _scale(self, data):
-        # For every feature column,
-        data[data.columns] = self.scaler.fit_transform(data[data.columns])
-
-        # Scale the target column
-        target_col = data.columns[-1]
-        data[[target_col]] = self.target_scaler.fit_transform(data[[target_col]])
-
-        return data
-    
-
-    def _build_windowed_X_y(self, window_length=5):
-        X = self.data.iloc[:,:-1].values
-        y = self.data.iloc[:,-1].values
-
-        num_samples = len(X) - window_length
-
-        windowed_X = []
-        windowed_y = []
-        for index in range(num_samples):
-            current_window_end = index + window_length
-            cur_X_seq = X[index:current_window_end, :]
-            windowed_X.append(cur_X_seq)
-
-            windowed_y.append(y[current_window_end])
-
-        X = np.array(windowed_X)
-        y = np.array(windowed_y)
-        
+    def _pre_process(self, df, fit_scalers=True):
+        print("Adding lag")
+        df = add_lag(df)
+        print("Splitting X, y")
+        X, y = split_X_y(df)
+        print("Scaling X, y")
+        X = scale(X, self.scaler, fit_scalers=fit_scalers)
+        y = scale(y, self.target_scaler, fit_scalers=fit_scalers)
         return (X, y)
 
-    def _partition(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size = 0.2, random_state = 0)
-        return (X_train, X_test, y_train, y_test)
+
+    @property
+    def X_train_shaped(self):
+        return self.X_train.reshape(self.X_train.shape[0], self.X_train.shape[1], self.X_train.shape[2], 1)
+    @property
+    def X_test_shaped(self):
+        return self.X_test.reshape(self.X_test.shape[0], self.X_test.shape[1], self.X_test.shape[2], 1)
+
+    @property
+    def input_shape(self):
+        print(self.X_train_shaped[0].shape)
+        return self.X_train_shaped[0].shape
