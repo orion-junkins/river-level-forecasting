eb0cce100e670447514ed84b8fcdd7b15c6ec6e8
orion-junkins <orionjunkins@gmail.com>
2022-02-08 18:22:12 -0800

    Catchment data added for initial five rivers

--- data/catchment_meta_data.py
+++ data/catchment_meta_data.py
@@ -1,6 +1,7 @@
 from data.weather_locations import all_weather_locs
+
 class Catchment:
-    def __init__(self, name, usgs_gauge_id, level_forecast_url, weather_locs=None) -> None:
+    def __init__(self, name, usgs_gauge_id, level_forecast_url=None, weather_locs=None) -> None:
         self.name = name
         self.usgs_gauge_id = str(usgs_gauge_id)
         self.level_forecast_url = level_forecast_url
@@ -10,6 +11,21 @@ class Catchment:
             self.weather_locs = weather_locs
 
 
+icicle_abv_snow = Catchment(name = "icicle-abv-snow",
+                        usgs_gauge_id = "12458000")
+
+white_salmon_underwood = Catchment(name = "white-salmon-underwood",
+                        usgs_gauge_id = "14123500")
+
+cispus_abv_yellowjacket = Catchment(name = "cispus-abv-yellowjacket",
+                        usgs_gauge_id = "14231900",
+                        level_forecast_url = "https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=ciyw1&output=tabular")
+
 illinois_kerby = Catchment(name = "illinois-kerby",
                         usgs_gauge_id="14377100", 
                         level_forecast_url = "https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=krbo3&output=tabular")
+
+
+mckenzie_vida = Catchment(name = "mckenzie-vida",
+                        usgs_gauge_id="14162500",
+                        level_forecast_url = "https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=vido3&output=tabular")
