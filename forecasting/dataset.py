0eb4948147b157fc684c3640a714b5afa43aa8db
orion-junkins <orionjunkins@gmail.com>
2022-02-26 13:09:33 -0800

    Simple feature engineering outlined for rolling windows

--- forecasting/dataset.py
+++ forecasting/dataset.py
@@ -11,14 +11,19 @@ class Dataset:
         self.scaler = Scaler(MinMaxScaler())
         self.target_scaler = Scaler(MinMaxScaler())
 
+
+        current_dfs = self.catchment_data.all_current_data.copy()
+        print("shape of current_dfs in Dataset init: ", current_dfs[0].shape)
+        self.Xs_current, self.y_current = self._pre_process(current_dfs, fit_scalers=False)
+
+
+
         historical_dfs = self.catchment_data.all_historical_data.copy()
         self.Xs_historical, self.y_historical = self._pre_process(historical_dfs)
 
         self.X_trains, self.X_tests, self.X_validations, self.y_train, self.y_test, self.y_validation = self._partition()
   
-        current_dfs = self.catchment_data.all_current_data.copy()
-        self.Xs_current, self.y_current = self._pre_process(current_dfs, fit_scalers=False)
-
+        
     @property
     def num_X_sets(self):
         return len(self.X_trains)
@@ -32,6 +37,7 @@ class Dataset:
         y = None
         for df in dfs:
             X_cur, y_cur = split_X_y(df)
+            X_cur = self.add_engineered_features(X_cur)
             X_cur = timeseries.TimeSeries.from_dataframe(X_cur)
             y_cur = timeseries.TimeSeries.from_dataframe(y_cur)
             if fit_scalers:
@@ -50,7 +56,7 @@ class Dataset:
 
         return Xs, y
 
-    def _partition(self, test_size=0.1, validation_size=0.2):
+    def _partition(self, test_size=0.2, validation_size=0.2):
         X_trains = []
         X_tests = []
         X_validations = []
@@ -66,3 +72,18 @@ class Dataset:
         y_train, y_validation = y_train.split_after(1-validation_size)
 
         return (X_trains, X_tests, X_validations, y_train, y_test, y_validation)
+    
+    def add_engineered_features(self, df):
+        df['day_of_year'] = df.index.day_of_year
+
+        df['snow_10d'] = df['snow_1h'].rolling(window=10 * 24).sum()
+        # df['snow_30d'] = df['snow_1h'].rolling(window=30 * 24).sum()
+        df['rain_10d'] = df['rain_1h'].rolling(window=10 * 24).sum()
+        # df['rain_30d'] = df['rain_1h'].rolling(window=30 * 24).sum()
+
+        df['temp_10d'] = df['temp'].rolling(window=10 * 24).mean()
+        # df['temp_30d'] = df['temp'].rolling(window=30 * 24).mean()
+        print(df.shape)
+        df.dropna(inplace=True)
+        print(df.shape)
+        return df
