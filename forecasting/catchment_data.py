0bfcc2f05d97a804e0cc513817e1460171ac9051
orion-junkins <orionjunkins@gmail.com>
2022-03-05 18:07:22 -0800

    Fetchers modified to return separate Xs and y in tuple

--- forecasting/catchment_data.py
+++ forecasting/catchment_data.py
@@ -21,9 +21,6 @@ class CatchmentData:
             self.historical_weather = fetch_all_historical_weather(self.weather_locs, self.name) 
         
         self.recent_level = get_historical_level(self.usgs_gauge_id, start=date_days_ago(self.window_size))
-
-        # Lists of weather dataframes (indexed by 'datetime', columns = ['temp', 'rain', etc.])
-        self.historical_weather = fetch_all_historical_weather(self.weather_locs, self.name) 
         self.recent_weather = fetch_all_recent_weather(self.weather_locs, start=unix_timestamp_days_ago(self.window_size))
         self.forecasted_weather = fetch_all_forecasted_weather(self.weather_locs)
 
@@ -35,24 +32,18 @@ class CatchmentData:
         all_current_data = []
         for df_recent, df_forecasted in zip(self.recent_weather, self.forecasted_weather):
             weather_frames = [df_recent, df_forecasted]
-            df_weather = pd.concat(weather_frames) # Combine recent and forecasted weather into a single df
-            df = pd.concat([df_weather, self.recent_level], axis=1, join='inner') # Add level data
+            df = pd.concat(weather_frames) # Combine recent and forecasted weather into a single df
             df = handle_missing_data(df)
             all_current_data.append(df)
         
-        return all_current_data
+        return (all_current_data, self.recent_level)
     
 
     @property
     def all_historical_data(self):
         """
         """
-        all_historical_data = []
-        for df_weather in self.historical_weather:
-            df = pd.concat([df_weather, self.historical_level], axis=1, join='inner') # Add level data
-            df = handle_missing_data(df)
-            all_historical_data.append(df)
-        return all_historical_data
+        return (self.historical_weather, self.historical_level)
         
         
     def update_for_inference(self):
