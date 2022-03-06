1f67b67c54d64946c09f44ec3d5b6cc43f161172
orion-junkins <orionjunkins@gmail.com>
2022-03-05 18:09:18 -0800

    Web app extended to more time ranges

--- web_app.py
+++ web_app.py
@@ -17,7 +17,7 @@ def test_historical(horizon, stride):
     catchment = pickle.load(pickle_in)
     block_rnn_forecaster = Forecaster(catchment, model_type=BlockRNNModel, model_save_dir="BlockRNN")
     y_pred = block_rnn_forecaster.historical_forecasts(forecast_horizon=horizon, stride=stride, num_samples=100)
-    y_true = block_rnn_forecaster.dataset.y_validation
+    y_true = block_rnn_forecaster.dataset.y_test
     target_scaler = block_rnn_forecaster.dataset.target_scaler
     y_true = target_scaler.inverse_transform(y_true)
     y_true = y_true.pd_dataframe()
@@ -29,10 +29,10 @@ st.title('River Level Forecasting with ML')
 
 start_time, end_time = st.slider(
      "Select start time",
-     min_value=datetime(2013, 1, 1, 9, 30),
-     value=(datetime(2014, 1, 1, 9, 30), datetime(2015, 1, 1, 9, 30)),
-     max_value=datetime(2015, 1, 1, 9, 30),
-     format="MM/DD/YY - hh:mm")
+     min_value=datetime(2018, 10, 26, 0, 0),
+     value=(datetime(2019, 1, 1, 9, 30), datetime(2020, 1, 1, 9, 30)),
+     max_value=datetime(2022, 2, 15, 0, 0),
+     format="MM/DD/YY")
 st.write("Start time:", start_time)
 st.write("End time:", end_time)
 start = pd.to_datetime(start_time)
@@ -49,19 +49,28 @@ def build_figure(hst_fcasts, start, end):
     plt.plot(hst_fcasts.index, hst_fcasts['level'],  color='blue', linewidth=2, label='Actual value')
     plt.legend()
 
-    y_lim = hst_fcasts.loc[start:end, :]['max'].max() * 1.2
+    y_lim = hst_fcasts.loc[start:end, :]['level'].max() * 1.2
     plt.xlim(start,end)
-    plt.ylim(-100, y_lim)
+    plt.ylim(-200, y_lim)
     return fig
 
 st.subheader('24 hour advance prediction window')
-hst_fcasts_24_4 = test_historical(24, 4)
+hst_fcasts_24_4 = test_historical(24, 1)
 st.pyplot(build_figure(hst_fcasts_24_4, start, end))
 
 st.subheader('48 hour advance prediction window')
-hst_fcasts_48_4 = test_historical(48, 4)
+hst_fcasts_48_4 = test_historical(48, 1)
 st.pyplot(build_figure(hst_fcasts_48_4, start, end))
 
 st.subheader('72 hour advance prediction window')
-hst_fcasts_72_4 = test_historical(72, 4)
-st.pyplot(build_figure(hst_fcasts_72_4))
+hst_fcasts_72_4 = test_historical(72, 1)
+st.pyplot(build_figure(hst_fcasts_72_4, start, end))
+
+st.subheader('96 hour advance prediction window')
+hst_fcasts_96_4 = test_historical(96, 1)
+st.pyplot(build_figure(hst_fcasts_96_4, start, end))
+
+# st.subheader('120 hour advance prediction window')
+# hst_fcasts_120_4 = test_historical(120, 1)
+# st.pyplot(build_figure(hst_fcasts_120_4, start, end))
+
