1adb1d6b879e88cb3dcc4ead17dc76cac648fb8c
orion-junkins <orionjunkins@gmail.com>
2022-04-09 08:21:53 -0700

    train and inference scripts updated

--- run_inference.py
+++ run_inference.py
@@ -1,41 +1,24 @@
 #%%
 import os
-import sys
 import pickle
+import boto3
 
-from darts.models import BlockRNNModel
-from forecasting.catchment_data import CatchmentData
-from forecasting.forecaster import Forecaster
+IN_PATH = os.path.join("data", "forecast_ensemble.pickle")
+OUT_PATH = os.path.join("data", "forecast_df.pickle")
 
+pickle_in = open(IN_PATH, "rb")
+frcstr = pickle.load(pickle_in)
 
-rebuild_catchment=False # 'True' causes refetching of all data, 'False' loads cached version from .pickle file
+y_pred = frcstr.forecast_for_hours(96)
+print(y_pred)
 
-if rebuild_catchment:
-    catchment = CatchmentData("illinois-kerby", "14377100") # Specify target USGS gauge name and ID
+# %%
+pickle_out = open(OUT_PATH, "wb")
+pickle.dump(y_pred, pickle_out)
+pickle_out.close()
+# %%
 
-    pickle_out = open("data/catchment.pickle", "wb")
-    pickle.dump(catchment, pickle_out)
-    pickle_out.close()
-else:
-    pickle_in = open("data/catchment.pickle", "rb")
-    catchment = pickle.load(pickle_in)
-
-
-model_builder = BlockRNNModel
-
-# Define desired number of models (must be the same as the number of catchments)
-NUM_MODELS = 12
-
-WORK_DIR = os.path.join("trained_catchment_models", "gridsearched_gru")
-
-catchment_models = []
-
-for i in range(NUM_MODELS):
-    model = model_builder.load_from_checkpoint(model_name=str(i), work_dir=WORK_DIR)
-    catchment_models.append(model)
-#%%
-frcstr = Forecaster(catchment, catchment_models)
-
-print(frcstr.forecast_for_hours())
+s3 = boto3.client('s3')
+s3.upload_file(OUT_PATH, 'generated-forecasts', 'forecast_dataframe.pickle')
 
 # %%
--- train_ensemble.py
+++ train_ensemble.py
@@ -1,6 +1,5 @@
 #%%
 import os
-import sys
 import pickle
 
 from darts.models import BlockRNNModel
@@ -21,11 +20,11 @@ else:
     catchment = pickle.load(pickle_in)
 
 
-
+#%%
 model_builder = BlockRNNModel
 
 # gridsearch optimized params for a Darts BlockRNNModel
-params = {'pl_trainer_kwargs': {'accelerator': 'gpu', 'gpus': [0]}, 
+best_params = {'pl_trainer_kwargs': {'accelerator': 'gpu', 'gpus': [0]}, 
     'n_epochs': 10, 
     'input_chunk_length': 120, 
     'output_chunk_length': 96, 
@@ -34,16 +33,25 @@ params = {'pl_trainer_kwargs': {'accelerator': 'gpu', 'gpus': [0]},
     'n_rnn_layers': 5, 
     'dropout': 0.01}
 
+test_params = {'pl_trainer_kwargs': {'accelerator': 'gpu', 'gpus': [0]}, 
+    'n_epochs': 1, 
+    'input_chunk_length': 12, 
+    'output_chunk_length': 2, 
+    'model': 'GRU', 
+    'hidden_size': 5, 
+    'n_rnn_layers': 1, 
+    'dropout': 0.01}
+
 
 # Define desired number of models (must be the same as the number of catchments)
 NUM_MODELS = 12
 
-WORK_DIR = os.path.join("trained_catchment_models", sys.argv[1])
+WORK_DIR = os.path.join("trained_catchment_models", "gru3")
 
 catchment_models = []
 
 for i in range(NUM_MODELS):
-    model = model_builder(work_dir=WORK_DIR, model_name=str(i), force_reset=True, save_checkpoints=True, **params)
+    model = model_builder(work_dir=WORK_DIR, model_name=str(i), force_reset=True, save_checkpoints=True, **best_params)
     catchment_models.append(model)
 #%%
 frcstr = Forecaster(catchment, catchment_models)
@@ -52,4 +60,13 @@ frcstr.fit()
 
 print(frcstr.forecast_for_hours())
 
+# %%
+pickle_out = open("data/forecast_ensemble.pickle", "wb")
+pickle.dump(frcstr, pickle_out)
+pickle_out.close()
+
+#%%
+for i in range(len(catchment_models)):
+    model_path = os.path.join(WORK_DIR, "SAVED", str(i) + ".pth.tar")
+    model.save_model(model_path)
 # %%
