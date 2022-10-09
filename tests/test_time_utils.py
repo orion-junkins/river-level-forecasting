a961eb11db55f10b9f7a78c6a3d2c2d82e193c91
Doug Dennis <douglasdennisjr@gmail.com>
2022-10-09 13:35:02 -0700

    make initial test file

---
+++ tests/test_time_utils.py
@@ -0,0 +1,25 @@
+import pytest
+
+from forecasting.general_utilities.time_utils import convert_timestamp_to_datetime
+
+
+@pytest.fixture
+def good_timestamp_int():
+    return 1641038400
+
+
+@pytest.fixture
+def good_timestamp_str(good_timestamp_int):
+    return str(good_timestamp_int)
+
+
+def test_convert_timestamp_to_datetime_int(good_timestamp_int):
+    expected_result = "2022-01-01 12:00:00"
+    actual_result = convert_timestamp_to_datetime(good_timestamp_int)
+    assert(actual_result == expected_result)
+
+
+def test_convert_timestamp_to_datetime_str(good_timestamp_str):
+    expected_result = "2022-01-01 12:00:00"
+    actual_result = convert_timestamp_to_datetime(good_timestamp_str)
+    assert(actual_result == expected_result)
