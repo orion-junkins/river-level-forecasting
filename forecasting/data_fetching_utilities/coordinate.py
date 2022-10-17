43bde392e6c745455692b48a210d5f722441e19a
Orion Junkins <orionjunkins@gmail.com>
2022-10-17 11:11:26 -0700

    Coordinate transitioned to namedTuple

--- forecasting/data_fetching_utilities/coordinate.py
+++ forecasting/data_fetching_utilities/coordinate.py
@@ -1,14 +1,5 @@
-class Coordinate:
-    """
-    Basic 2-space coordinate
-    """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+from collections import namedtuple
 
-    def __repr__(self):
-        return ("(" + str(self.x) + ", " + str(self.y) + ")")
-
-        
-    def __str__(self):
-        return ("(" + str(self.x) + ", " + str(self.y) + ")")
+# Create a namedtuple type, Point
+Coordinate = namedtuple("Coordinate", "lon lat")
+issubclass(Coordinate, tuple)
