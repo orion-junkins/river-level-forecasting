72783b817a6c3defcce7e81ab73f6e34e547d599
Zach Bochanski <ztbochanski@gmail.com>
2022-10-20 11:39:47 -0700

    refactor hourly to be more specific to parameters

--- forecasting/data_fetching_utilities/data_providers/weather_provider.py
+++ forecasting/data_fetching_utilities/data_providers/weather_provider.py
@@ -1,7 +1,7 @@
 from forecasting.data_fetching_utilities.api.api import RequestBuilder
 from forecasting.data_fetching_utilities.open_meteo.models import ResponseModel
 from forecasting.data_fetching_utilities.open_meteo.open_meteo_adapter import OpenMeteoAdapter
-from forecasting.data_fetching_utilities.open_meteo.hourly import Hourly
+from forecasting.data_fetching_utilities.open_meteo.open_meteo_hourly_parameters import OpenMeteoHourlyParameters
 
 import pandas as pd
 
@@ -15,18 +15,25 @@ class WeatherProvider():
         self.locations = locations
 
     def fetch_historical_weather(self) -> pd.DataFrame:
-        open_meteo_archive = OpenMeteoAdapter(hostname="archive-api.open-meteo.com", latitude=self.locations[0][0], longitude=self.locations[0][1],
-                                              start_date=TEST_DATE_MIN, end_date=TEST_DATE_MAX,)
-        hourly = Hourly()
-        hourly.set_all()
 
-        open_meteo_archive.set_hourly(hourly=hourly.get_variables())
-        request_builder = RequestBuilder(api_adapter=open_meteo_archive)
+        open_meteo_archive_api_adapter = OpenMeteoAdapter(hostname="archive-api.open-meteo.com", latitude=self.locations[0][0], longitude=self.locations[0][1],
+                                                          start_date=TEST_DATE_MIN, end_date=TEST_DATE_MAX,)
+
+        hourly_parameters = OpenMeteoHourlyParameters()
+        hourly_parameters.set_all()
+
+        open_meteo_archive_api_adapter.set_hourly_parameters(
+            hourly_parameters=hourly_parameters.get_variables())
+
+        request_builder = RequestBuilder(
+            api_adapter=open_meteo_archive_api_adapter)
+
         response = request_builder.get()
         print("Message:", response.message,
               "Status Code:", response.status_code)
 
         response_model = ResponseModel(**response.data)
+        print(response_model.hourly)
         df = pd.DataFrame(response_model.hourly.__dict__)
         df["latitude"] = response_model.latitude
         df["longitude"] = response_model.longitude
