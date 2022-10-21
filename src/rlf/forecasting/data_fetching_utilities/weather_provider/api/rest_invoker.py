49a56d706065861e0957cb9dc7dd1dadc9d64a3a
Zach Bochanski <ztbochanski@gmail.com>
2022-10-21 16:20:30 -0700

    update imports

--- src/rlf/forecasting/data_fetching_utilities/weather_provider/api/rest_invoker.py
+++ src/rlf/forecasting/data_fetching_utilities/weather_provider/api/rest_invoker.py
@@ -1,6 +1,7 @@
 import requests
-from rlf.forecasting.data_fetching_utilities.weather_provider.api.exceptions import RestInvokerException
+
 from rlf.forecasting.data_fetching_utilities.weather_provider.api.models import Response
+from rlf.forecasting.data_fetching_utilities.weather_provider.api.exceptions import RestInvokerException
 
 
 class RestInvoker():
