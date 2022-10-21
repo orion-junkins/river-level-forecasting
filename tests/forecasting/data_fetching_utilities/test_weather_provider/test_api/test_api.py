ec9bdca5708df9a9f6604730ccf11a983c1c3e67
Zach Bochanski <ztbochanski@gmail.com>
2022-10-21 15:59:04 -0700

    remove class and low value tests

--- tests/forecasting/data_fetching_utilities/test_weather_provider/test_api/test_api.py
+++ tests/forecasting/data_fetching_utilities/test_weather_provider/test_api/test_api.py
@@ -1,66 +1,56 @@
 import pytest
 
-from rlf.forecasting.data_fetching_utilities.open_meteo.open_meteo_adapter import OpenMeteoAdapter
-from rlf.forecasting.data_fetching_utilities.api.api import RequestBuilder
-from rlf.forecasting.data_fetching_utilities.api.models import Response
+from rlf.forecasting.data_fetching_utilities.weather_provider.open_meteo.open_meteo_adapter import OpenMeteoAdapter
+from rlf.forecasting.data_fetching_utilities.weather_provider.api.api import RequestBuilder
+from rlf.forecasting.data_fetching_utilities.weather_provider.api.models import Response
 
 
-class TestRequestBuilder:
+@pytest.fixture
+def fake_latitude() -> float:
+    return 1.0
 
-    @pytest.fixture
-    def open_meteo_adapter(self):
-        return OpenMeteoAdapter()
 
-    @pytest.fixture
-    def request_builder(self, open_meteo_adapter):
-        return RequestBuilder(open_meteo_adapter)
+@pytest.fixture
+def fake_longitude() -> float:
+    return 2.0
 
-    def test_init(self, request_builder):
-        try:
-            request_builder
-        except Exception:
-            pytest.fail("Failed to initialize RequestBuilder")
 
-    def test_has_attributes(self, request_builder):
-        assert hasattr(request_builder, "ssl_verify")
-        assert hasattr(request_builder, "protocol")
-        assert hasattr(request_builder, "hostname")
-        assert hasattr(request_builder, "version")
-        assert hasattr(request_builder, "path")
-        assert hasattr(request_builder, "parameters")
+@pytest.fixture
+def fake_start_date() -> str:
+    return "2022-08-14"
 
-    def test_has_api_adapter(self, request_builder):
-        assert request_builder.api_adapter is not None
 
-    def test_has_get_method(self, request_builder):
-        assert hasattr(request_builder, "get")
+@pytest.fixture
+def fake_end_date() -> str:
+    return "2022-09-14"
 
-    def test_get_returns_response(self, request_builder):
-        response = request_builder.get()
-        assert isinstance(response, Response)
 
-    def test_get_returns_response_with_status_code(self, request_builder):
-        response = request_builder.get()
-        assert response.status_code is not None
+@pytest.fixture
+def open_meteo_adapter():
+    return OpenMeteoAdapter(longitude=fake_longitude, latitude=fake_latitude, start_date=fake_start_date, end_date=fake_end_date)
 
-    def test_get_returns_response_with_content(self, request_builder):
-        response = request_builder.get()
-        assert response.data is not None
 
-    def test_has_parse_payload_method(self, request_builder):
-        assert hasattr(request_builder, "parse_payload")
+@pytest.fixture
+def request_builder(open_meteo_adapter):
+    return RequestBuilder(open_meteo_adapter)
 
-    def test_has_get_payload_method(self, request_builder):
-        assert hasattr(request_builder, "get_payload")
 
-    def test_api_adapter_can_get_payload(self, open_meteo_adapter):
-        payload = open_meteo_adapter.get_payload()
-        assert payload is not None
-        assert isinstance(payload, dict)
+def test_get_returns_response(request_builder):
+    response = request_builder.get()
+    assert isinstance(response, Response)
 
-    def test_parse_payload_sets_attributes_same_as_incoming_adapter(self, request_builder, open_meteo_adapter):
-        request_builder.parse_payload()
-        assert request_builder.protocol == open_meteo_adapter.protocol
-        assert request_builder.hostname == open_meteo_adapter.hostname
-        assert request_builder.version == open_meteo_adapter.version
-        assert request_builder.parameters == open_meteo_adapter.get_parameters()
+
+def test_get_returns_response_with_status_code(request_builder):
+    response = request_builder.get()
+    assert response.status_code is not None
+
+
+def test_get_returns_response_with_content(request_builder):
+    response = request_builder.get()
+    assert response.data is not None
+
+
+def test_api_adapter_can_get_payload(open_meteo_adapter):
+    payload = open_meteo_adapter.get_payload()
+    assert payload is not None
+    assert isinstance(payload, dict)
