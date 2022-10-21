4126c092a5593eb57c1bf0dde7ea2f2a78ef117d
Zach Bochanski <ztbochanski@gmail.com>
2022-10-20 17:06:26 -0700

    add method to fetch historical and return datum, in a list of datums

--- forecasting/data_fetching_utilities/data_providers/weather_provider.py
+++ forecasting/data_fetching_utilities/data_providers/weather_provider.py
@@ -2,22 +2,53 @@ from forecasting.data_fetching_utilities.api.api import RequestBuilder
 from forecasting.data_fetching_utilities.open_meteo.models import ResponseModel
 from forecasting.data_fetching_utilities.open_meteo.open_meteo_adapter import OpenMeteoAdapter
 from forecasting.data_fetching_utilities.open_meteo.open_meteo_hourly_parameters import OpenMeteoHourlyParameters
+from forecasting.data_fetching_utilities.data_providers.datum import Datum
 
 import pandas as pd
 
-TEST_DATE_MIN = "2021-01-01"
-TEST_DATE_MAX = "2021-01-02"
-
 
 class WeatherProvider():
+    """Provides a historical of forecasted weather for a given location and time period"""
 
     def __init__(self, locations: list[tuple: float, float]):
+        """Takes a list of tuples of longitude and latitude
+
+        Args:
+            locations (list[tuple: (float, float)]): WSG84 coordinates: (longitude, latitude)
+        """
         self.locations = locations
 
-    def fetch_historical_weather(self) -> pd.DataFrame:
+    def fetch_historical_weather(self, start_date: str = "2021-01-01", end_date: str = "2021-01-02") -> list[Datum]:
+        """Fetch historical weather for all locations
 
-        open_meteo_archive_api_adapter = OpenMeteoAdapter(hostname="archive-api.open-meteo.com", latitude=self.locations[0][0], longitude=self.locations[0][1],
-                                                          start_date=TEST_DATE_MIN, end_date=TEST_DATE_MAX,)
+        Args:
+            start_date (str, optional):  iso8601 format YYYY-MM-DD (https://en.wikipedia.org/wiki/ISO_8601). Defaults to "2021-01-01".
+            end_date (str, optional): iso8601 format YYYY-MM-DD. Defaults to "2021-01-02".
+
+        Returns:
+            list[Datum]: A list of Datum objects containing the weather data and metadata about the location or datum
+        """
+        datums = []
+        for location in self.locations:
+            datum = self.fetch_historical_weather_at_datum(
+                longitude=location[0], latitude=location[1], start_date=start_date, end_date=end_date)
+            datums.append(datum)
+        return datums
+
+    def fetch_historical_weather_at_datum(self, longitude: float, latitude: float, start_date: str, end_date: str) -> Datum:
+        """Fetch historical weather for a single location or datum
+
+        Args:
+            longitude (float): WSG84 longitude
+            latitude (float): WSG84 latitude
+            start_date (str): iso8601 format YYYY-MM-DD
+            end_date (str): iso8601 format YYYY-MM-DD
+
+        Returns:
+            Datum: A Datum object containing the weather data and metadata about a location (https://en.wikipedia.org/wiki/Geodetic_datum)
+        """        """"""
+        open_meteo_archive_api_adapter = OpenMeteoAdapter(hostname="archive-api.open-meteo.com", latitude=latitude, longitude=longitude,
+                                                          start_date=start_date, end_date=end_date,)
 
         hourly_parameters = OpenMeteoHourlyParameters()
         hourly_parameters.set_all()
@@ -33,16 +64,12 @@ class WeatherProvider():
               "Status Code:", response.status_code)
 
         response_model = ResponseModel(**response.data)
-        print(response_model.hourly)
-        df = pd.DataFrame(response_model.hourly_parameters())
-        df["latitude"] = response_model.latitude
-        df["longitude"] = response_model.longitude
-        df["generationtime_ms"] = response_model.generationtime_ms
-        df["utc_offset_seconds"] = response_model.utc_offset_seconds
-        df["timezone"] = response_model.timezone
-        df["timezone_abbreviation"] = response_model.timezone_abbreviation
-        df["elevation"] = response_model.elevation
-        return df
+
+        datum = Datum(longitude=response_model.longitude, latitude=response_model.latitude,
+                      elevation=response_model.elevation, utc_offset_seconds=response_model.utc_offset_seconds,
+                      timezone=response_model.timezone, hourly_parameters=response_model.hourly_parameters())
+
+        return datum
 
     def fetch_current_weather(self, past_hours_to_fetch, future_hours_to_fetch) -> pd.DataFrame:
         pass
--- tests/test_data_providers/test_weather_provider.py
+++ tests/test_data_providers/test_weather_provider.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
 from forecasting.data_fetching_utilities.data_providers.weather_provider import WeatherProvider
+from forecasting.data_fetching_utilities.data_providers.datum import Datum
 
 
 class TestWeatherProvider():
@@ -15,7 +16,9 @@ class TestWeatherProvider():
         provider = WeatherProvider([(5.55, 5.55), (5.55, 5.55)])
         assert provider.locations == [(5.55, 5.55), (5.55, 5.55)]
 
-    def test_fetch_historical_weather(self):
+    # returns a list of Datum objects
+    def test_fetch_historical_weather_returns_list_of_datums(self):
         provider = WeatherProvider([(5.55, 5.55), (5.55, 5.55)])
-        provider.fetch_historical_weather()
-        assert isinstance(provider.fetch_historical_weather(), pd.DataFrame)
+        datums = provider.fetch_historical_weather()
+        assert isinstance(datums, list)
+        assert isinstance(datums[0], Datum)
