14f1e8c5e8a937f41b12c5c4d6cf416588a7406f
Zach Bochanski <ztbochanski@gmail.com>
2022-10-21 15:50:42 -0700

    remove low value tests add fake

--- tests/forecasting/data_fetching_utilities/test_weather_provider/test_weather_provider.py
+++ tests/forecasting/data_fetching_utilities/test_weather_provider/test_weather_provider.py
@@ -1,24 +1,32 @@
 import pytest
-import pandas as pd
-from rlf.forecasting.data_fetching_utilities.data_providers.weather_provider import WeatherProvider
-from rlf.forecasting.data_fetching_utilities.data_providers.datum import Datum
+from rlf.forecasting.data_fetching_utilities.weather_provider.datum import Datum
+from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
 
 
-class TestWeatherProvider():
+class FakeWeatherProvider():
+    def __init__(self, coordinates: Coordinate):
+        self.coordinates = coordinates
 
-    def test_initialization(self):
-        try:
-            WeatherProvider(locations=[(5.55, 5.55), (5.55, 5.55)])
-        except NameError:
-            pytest.fail("WeatherProvider failed to initialize")
+    def fetch_historical_weather(self, start_date: str, end_date: str) -> list[Datum]:
+        return [Datum(longitude=1.0, latitude=1.0, elevation=3.0,
+                      utc_offset_seconds=4.0, timezone="Time Zone", hourly_parameters={})]
 
-    def test_has_locations(self):
-        provider = WeatherProvider([(5.55, 5.55), (5.55, 5.55)])
-        assert provider.locations == [(5.55, 5.55), (5.55, 5.55)]
 
-    # returns a list of Datum objects
-    def test_fetch_historical_weather_returns_list_of_datums(self):
-        provider = WeatherProvider([(5.55, 5.55), (5.55, 5.55)])
-        datums = provider.fetch_historical_weather()
-        assert isinstance(datums, list)
-        assert isinstance(datums[0], Datum)
+@ pytest.fixture
+def weather_provider():
+    return FakeWeatherProvider(coordinates=[Coordinate(lon=1.0, lat=2.0), Coordinate(lon=3.0, lat=4.0)])
+
+
+def test_weather_provider_accepts_coordinates_as_list():
+    try:
+        FakeWeatherProvider(coordinates=[Coordinate(
+            lon=1.0, lat=2.0), Coordinate(lon=3.0, lat=4.0)])
+    except TypeError:
+        pytest.fail("Failed to accept a list of Coordinates")
+
+
+def test_fetch_historical_weather_returns_list_of_datums(weather_provider):
+    datums = weather_provider.fetch_historical_weather(
+        start_date="start", end_date="end")
+    assert isinstance(datums, list)
+    assert isinstance(datums[0], Datum)
