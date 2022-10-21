653c889e32ad196aef9fa4371b9d2b0ed5aa3011
Zach Bochanski <ztbochanski@gmail.com>
2022-10-21 15:50:27 -0700

    remove low value tests

--- tests/forecasting/data_fetching_utilities/test_weather_provider/test_datum.py
+++ tests/forecasting/data_fetching_utilities/test_weather_provider/test_datum.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from rlf.forecasting.data_fetching_utilities.data_providers.datum import Datum
+from rlf.forecasting.data_fetching_utilities.weather_provider.datum import Datum
 
 
 class TestDatum():
@@ -10,12 +10,6 @@ class TestDatum():
     def datum(self):
         return Datum(longitude=0, latitude=0, elevation=0, utc_offset_seconds=0,
                      timezone="UTC", hourly_parameters={})
-
-    def test_initialization(self, datum):
-        try:
-            datum
-        except Exception as e:
-            pytest.fail("Datum() raised an exception: " + str(e))
 
     def test_get_hourly_parameters_returns_dict(self, datum):
         assert isinstance(datum.get_hourly_parameters(
@@ -28,9 +22,6 @@ class TestDatum():
     def test_get_hourly_parameters_returns_dataframe_with_correct_columns(self, datum):
         assert datum.get_hourly_parameters(
             as_pandas_data_frame=True).columns.tolist() == list(datum.hourly_parameters.keys())
-
-    def test_has_hourly_parameters(self, datum):
-        assert datum.hourly_parameters == {}
 
     def test_returns_longitude(self, datum):
         assert datum.get_longitude() == 0
