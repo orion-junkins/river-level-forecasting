ca3d20c9f1e3df0359302d7919f2ca8c9d723d2f
Zach Bochanski <ztbochanski@gmail.com>
2022-10-21 14:39:53 -0700

    remove low value tests

--- tests/forecasting/data_fetching_utilities/test_weather_provider/test_open_meteo/test_open_meteo_adapter.py
+++ tests/forecasting/data_fetching_utilities/test_weather_provider/test_open_meteo/test_open_meteo_adapter.py
@@ -1,32 +1,16 @@
 import pytest
-from rlf.forecasting.data_fetching_utilities.open_meteo.open_meteo_adapter import OpenMeteoAdapter
+from rlf.forecasting.data_fetching_utilities.weather_provider.open_meteo.open_meteo_adapter import OpenMeteoAdapter
 
 
 class TestOpenMeteoAdapter():
 
     @pytest.fixture
     def fake_latitude(self) -> float:
-        return 55.55
-
-    @pytest.fixture
-    def fake_min_invalid_latitude(self) -> float:
-        return -100
-
-    @pytest.fixture
-    def fake_max_invalid_latitude(self) -> float:
-        return 100
-
-    @pytest.fixture
-    def fake_min_invalid_longitude(self) -> float:
-        return -200
-
-    @pytest.fixture
-    def fake_max_invalid_longitude(self) -> float:
-        return 200
+        return 1.0
 
     @pytest.fixture
     def fake_longitude(self) -> float:
-        return 55.55
+        return 2.0
 
     @pytest.fixture
     def fake_start_date(self) -> str:
@@ -37,32 +21,8 @@ class TestOpenMeteoAdapter():
         return "2022-09-14"
 
     @pytest.fixture
-    def weather_api_parameters(self) -> OpenMeteoAdapter:
-        return OpenMeteoAdapter()
-
-    def test_has_base_url_attributes(self, weather_api_parameters):
-        assert hasattr(weather_api_parameters, "protocol")
-        assert hasattr(weather_api_parameters, "hostname")
-        assert hasattr(weather_api_parameters, "version")
-        assert hasattr(weather_api_parameters, "path")
-
-    def test_has_latitude(self, weather_api_parameters):
-        assert (weather_api_parameters.latitude)
-
-    def test_has_longitude(self, weather_api_parameters):
-        assert (weather_api_parameters.longitude)
-
-    def test_has_start_date(self, weather_api_parameters):
-        assert hasattr(weather_api_parameters, "start_date")
-
-    def test_has_end_date(self, weather_api_parameters):
-        assert hasattr(weather_api_parameters, "end_date")
-
-    def test_has_hourly_parameter(self, weather_api_parameters):
-        assert hasattr(weather_api_parameters, "hourly_parameters")
-
-    def test_has_get_payload_method(self, weather_api_parameters):
-        assert hasattr(weather_api_parameters, "get_payload")
+    def weather_api_parameters(self, fake_latitude, fake_longitude, fake_start_date, fake_end_date) -> OpenMeteoAdapter:
+        return OpenMeteoAdapter(longitude=fake_longitude, latitude=fake_latitude, start_date=fake_start_date, end_date=fake_end_date)
 
     def test_get_payload_returns_dict(self, weather_api_parameters):
         assert isinstance(weather_api_parameters.get_payload(), dict)
@@ -76,15 +36,6 @@ class TestOpenMeteoAdapter():
             "parameters": weather_api_parameters.get_parameters()
         })
 
-    def test_min_valid_latitude(self, fake_min_invalid_latitude):
-        with pytest.raises(ValueError):
-            OpenMeteoAdapter(latitude=fake_min_invalid_latitude)
-
-    def test_set_location(self, fake_latitude, fake_longitude, weather_api_parameters):
-        weather_api_parameters.set_location(fake_latitude, fake_longitude)
-        assert (weather_api_parameters.latitude == fake_latitude)
-        assert (weather_api_parameters.longitude == fake_longitude)
-
     def test_get_location_returns_tuple(self, weather_api_parameters):
         assert isinstance(weather_api_parameters.get_location(), tuple)
 
@@ -97,9 +48,7 @@ class TestOpenMeteoAdapter():
         weather_api_parameters.set_start_date(fake_start_date)
         assert (weather_api_parameters.get_start_date() == fake_start_date)
 
-    def test_get_start_date(self, fake_start_date):
-        weather_api_parameters = OpenMeteoAdapter(
-            start_date=fake_start_date)
+    def test_get_start_date(self, weather_api_parameters, fake_start_date):
         assert (weather_api_parameters.get_start_date() == fake_start_date)
 
     def test_set_end_date(self, fake_end_date, weather_api_parameters):
