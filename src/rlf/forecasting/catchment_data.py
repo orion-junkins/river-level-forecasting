3b4d1d2e104c18288549c04692ddbbbc8f1eff7b
orion-junkins <orionjunkins@gmail.com>
2022-10-23 12:19:42 -0700

    Typehint removed from CatchmentData for now

--- src/rlf/forecasting/catchment_data.py
+++ src/rlf/forecasting/catchment_data.py
@@ -1,8 +1,5 @@
-from rlf.forecasting.data_fetching_utilities.level_provider.level_provider_abc import BaseLevelProvider
-
-
 class CatchmentData:
-    def __init__(self, catchment_name, weather_provider, level_provider: BaseLevelProvider, num_recent_samples=40*24) -> None:
+    def __init__(self, catchment_name, weather_provider, level_provider, num_recent_samples=40*24) -> None:
         self.name = catchment_name
         self.weather_provider = weather_provider
         self.level_provider = level_provider
