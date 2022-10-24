dc05053a754d0ba0e73b182535104ea967a84558
Zach Bochanski <ztbochanski@gmail.com>
2022-10-24 15:43:16 -0700

    add additional coord to demo for weather provider

--- demos/demo_weather_provider.py
+++ demos/demo_weather_provider.py
@@ -3,15 +3,15 @@
 from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
 from rlf.forecasting.data_fetching_utilities.weather_provider.weather_provider import WeatherProvider
 
-coordinates = [Coordinate(lat=52.52, lon=300)]
+coordinates = [Coordinate(lat=52.52, lon=30),
+               Coordinate(lat=52.52, lon=30.1), ]
 
 weather_provider = WeatherProvider(coordinates=coordinates)
 wx = weather_provider.fetch_historical_weather(
     start_date="2021-01-01", end_date="2021-01-02")
-print(type(wx))
 
 # %%
 for datum in wx:
-    print(datum.get_data_frame())
+    print(datum)
 
 # %%
