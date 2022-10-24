169e8ebf4af06921b4927fbaa400c4342b3ffd23
Zach Bochanski <ztbochanski@gmail.com>
2022-10-24 15:46:19 -0700

    convert response into dataclass

--- src/rlf/forecasting/data_fetching_utilities/weather_provider/api/models.py
+++ src/rlf/forecasting/data_fetching_utilities/weather_provider/api/models.py
@@ -1,19 +1,19 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Response():
     """Response object from a REST API
+
+    Args:
+        status_code (int): The status code of the response
+        url (str): The URL of the response
+        message (str): The message of the response
+        headers (dict): The headers of the response
+        data (dict, optional): The data of the response. Defaults to None.
     """
-
-    def __init__(self, status_code: int, url: str, message: str, headers: dict, data: list[dict] = None):
-        """Response object from a REST API
-
-        Args:
-            status_code (int): The status code of the response
-            url (str): The URL of the response
-            message (str): The message of the response
-            headers (dict): The headers of the response
-            data (list[dict], optional): The data of the response. Defaults to None.
-        """
-        self.status_code = status_code
-        self.url = url
-        self.message = message
-        self.headers = headers
-        self.data = data if data is not None else []
+    status_code: int
+    url: str
+    message: str
+    headers: dict
+    data: dict = None
