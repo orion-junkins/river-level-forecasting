939da3ead5b75d03882811f085c008a4f8a10b1f
Zach Bochanski <ztbochanski@gmail.com>
2022-10-25 13:15:49 -0700

    add tests to test weather provider
    use a fake api adapter for the weather provider to return stubbed info

--- tests/forecasting/data_fetching_utilities/weather_provider/test_weather_provider.py
+++ tests/forecasting/data_fetching_utilities/weather_provider/test_weather_provider.py
@@ -1,35 +1,78 @@
 import pytest
 
 from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
+from rlf.forecasting.data_fetching_utilities.weather_provider.api.base_api_adapter import BaseAPIAdapter
 from rlf.forecasting.data_fetching_utilities.weather_provider.weather_datum import WeatherDatum
+from rlf.forecasting.data_fetching_utilities.weather_provider.api.models import Response
+from rlf.forecasting.data_fetching_utilities.weather_provider.weather_provider import WeatherProvider
 
 
-class FakeWeatherProvider():
-    def __init__(self, coordinates: Coordinate):
-        self.coordinates = coordinates
+class FakeWeatherAPIAdapter(BaseAPIAdapter):
 
-    def fetch_historical_weather(self, start_date: str, end_date: str) -> list[WeatherDatum]:
-        return [WeatherDatum(longitude=1.0, latitude=1.0, elevation=3.0,
-                             utc_offset_seconds=4.0, timezone="Fake Time Zone", hourly_units="Fake Units", hourly_parameters={})]
+    def __init__(self,
+                 longitude: float,
+                 latitude: float,
+                 start_date: str,
+                 end_date: str,
+                 protocol: str = "fake protocol",
+                 hostname: str = "fake hostname",
+                 version: str = "fake version",
+                 path: str = "fake path",
+                 hourly_parameters: list[str] = ["fake weather variable"]) -> None:
+        self.longitude = longitude
+        self.latitude = latitude
+        self.start_date = start_date
+        self.end_date = end_date
+        self.protocol = protocol
+        self.hostname = hostname
+        self.version = version
+        self.path = path
+        self.hourly_parameters = hourly_parameters
+
+    def get(self) -> Response:
+        response = Response(status_code=0,
+                            url="fake url",
+                            message="fake message",
+                            headers={"fake": "headers"},
+                            data={"longitude": self.longitude,
+                                  "latitude": self.latitude,
+                                  "hourly": self.hourly_parameters})
+        return response
+
+
+@pytest.fixture
+def fake_weather_api_adapter() -> FakeWeatherAPIAdapter:
+    return FakeWeatherAPIAdapter(longitude=0.0, latitude=0.0, start_date="fake start", end_date="fake end")
 
 
 @ pytest.fixture
-def weather_provider():
-    return FakeWeatherProvider(coordinates=[Coordinate(lon=1.0, lat=2.0), Coordinate(lon=3.0, lat=4.0)])
+def weather_provider(fake_weather_api_adapter) -> WeatherProvider:
+    coordinates = [Coordinate(lon=1.0, lat=2.0),
+                   Coordinate(lon=3.0, lat=4.0)]
+    weather_provider = WeatherProvider(
+        coordinates=coordinates, api_adapter=fake_weather_api_adapter)
+    return weather_provider
 
 
-def test_weather_provider_accepts_coordinates_as_list():
-    try:
-        FakeWeatherProvider(coordinates=[Coordinate(
-            lon=1.0, lat=2.0), Coordinate(lon=3.0, lat=4.0)])
-    except TypeError:
-        pytest.fail("Failed to accept a list of Coordinates")
-
-# fix
+def test_fetch_historical_input_output_equal(weather_provider):
+    weather_datums = weather_provider.fetch_historical_weather(
+        start_date="fake start", end_date="fake end")
+    assert len(weather_datums) == 2
 
 
-def test_fetch_historical_weather_returns_list_of_datums(weather_provider):
-    datums = weather_provider.fetch_historical_weather(
-        start_date="start", end_date="end")
-    assert isinstance(datums, list)
-    assert isinstance(datums[0], WeatherDatum)
+def test_fetch_historical_returns_weather_datum(weather_provider):
+    weather_datums = weather_provider.fetch_historical_weather(
+        start_date="fake start", end_date="fake end")
+    assert isinstance(weather_datums[0], WeatherDatum)
+
+
+def test_fetch_historical_returns_datum_at_input_location(weather_provider):
+    weather_datums = weather_provider.fetch_historical_weather(
+        start_date="fake start", end_date="fake end")
+    assert weather_datums[0].longitude == 1.0
+    assert weather_datums[0].latitude == 2.0
+    assert weather_datums[0].hourly_parameters == ["fake weather variable"]
+
+    assert weather_datums[1].longitude == 3.0
+    assert weather_datums[1].latitude == 4.0
+    assert weather_datums[1].hourly_parameters == ["fake weather variable"]
