c3653e9487458576e01376c12186d07dcb1351a6
Zach Bochanski <ztbochanski@gmail.com>
2022-10-25 13:03:44 -0700

    refactor demo provider to accept api adapters

--- demos/demo_weather_provider.py
+++ demos/demo_weather_provider.py
@@ -2,16 +2,20 @@
 # %%
 from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
 from rlf.forecasting.data_fetching_utilities.weather_provider.weather_provider import WeatherProvider
+from rlf.forecasting.data_fetching_utilities.weather_provider.open_meteo.open_meteo_adapter import OpenMeteoAdapter
 
 coordinates = [Coordinate(lat=52.52, lon=30),
-               Coordinate(lat=52.52, lon=30.1), ]
+               Coordinate(lat=52.52, lon=30.1)]
 
-weather_provider = WeatherProvider(coordinates=coordinates)
-wx = weather_provider.fetch_historical_weather(
-    start_date="2021-01-01", end_date="2021-01-02")
+weather_provider = WeatherProvider(
+    coordinates=coordinates, api_adapter=OpenMeteoAdapter())
+list_of_points_with_weather = weather_provider.fetch_historical_weather(
+    start_date="2021-01-01", end_date="2021-01-01")
 
 # %%
-for datum in wx:
+print(type(list_of_points_with_weather[0]))
+print(len(list_of_points_with_weather))
+for datum in list_of_points_with_weather:
     print(datum)
 
 # %%
