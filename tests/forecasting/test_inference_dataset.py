a789ab226a27b6501cebb4e724738e267937bee1
orion-junkins <orionjunkins@gmail.com>
2022-10-26 19:17:54 -0700

    Inference Dataset tests improved and passing

--- tests/forecasting/test_inference_dataset.py
+++ tests/forecasting/test_inference_dataset.py
@@ -8,7 +8,7 @@ from fake_providers import FakeLevelProvider, FakeWeatherProvider
 
 @pytest.fixture
 def catchment_data():
-    CatchmentData("test_catchment", FakeWeatherProvider(), FakeLevelProvider())
+    return CatchmentData("test_catchment", FakeWeatherProvider(num_historical_samples=1000), FakeLevelProvider(num_historical_samples=1000))
 
 
 @pytest.fixture
@@ -22,8 +22,8 @@ def scalers(training_dataset):
 
 
 @pytest.fixture
-def inference_dataset(scalers):
-    return InferenceDataset(scaler=scalers[0], target_scaler=scalers[1])
+def inference_dataset(scalers, catchment_data):
+    return InferenceDataset(catchment_data=catchment_data, scaler=scalers[0], target_scaler=scalers[1])
 
 
 def test_data_scaled_0_1(inference_dataset):
@@ -35,18 +35,3 @@ def test_data_scaled_0_1(inference_dataset):
     for x in inference_dataset.Xs:
         assert (x.values().min() >= -0.1)
         assert (x.values().max() <= 1.1)
-
-
-def test_feature_engineering(catchment_data):
-    column = "weather_attr_1"
-    rolling_window_size = 3
-    rolling_sum_column = column + "_sum_" + str(rolling_window_size)
-    rolling_mean_column = column + "_mean_" + str(rolling_window_size)
-
-    train_ds = InferenceDataset(catchment_data=catchment_data, rolling_sum_columns=[column], rolling_mean_columns=[column], rolling_window_sizes=[rolling_window_size])
-
-    for x in train_ds.Xs:
-        x_df = x.pd_dataframe()
-        assert (x_df[column][-rolling_window_size:].sum() == x_df[rolling_sum_column][-1])
-
-        assert (x_df[column][-rolling_window_size:].mean() == x_df[rolling_mean_column][-1])
