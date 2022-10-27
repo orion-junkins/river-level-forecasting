0bace5bc8a66d700d65d3e8968aa8c91b5b637c3
orion-junkins <orionjunkins@gmail.com>
2022-10-26 19:48:42 -0700

    Init tests for both forecaster variants

---
+++ tests/forecasting/test_training_forecaster.py
@@ -0,0 +1,22 @@
+import pytest
+
+from rlf.forecasting.catchment_data import CatchmentData
+from rlf.forecasting.training_dataset import TrainingDataset
+from rlf.forecasting.training_forecaster import TrainingForecaster
+from fake_providers import FakeLevelProvider, FakeWeatherProvider
+
+
+@pytest.fixture
+def catchment_data():
+    return CatchmentData("test_catchment", FakeWeatherProvider(num_historical_samples=1000), FakeLevelProvider(num_historical_samples=1000))
+
+
+@pytest.fixture
+def training_dataset(catchment_data):
+    return TrainingDataset(catchment_data=catchment_data)
+
+
+def test_training_forecaster_init(training_dataset, catchment_data):
+    training_forecaster = TrainingForecaster(catchment_data=catchment_data, dataset=training_dataset)
+
+    assert (type(training_forecaster.dataset) is TrainingDataset)
