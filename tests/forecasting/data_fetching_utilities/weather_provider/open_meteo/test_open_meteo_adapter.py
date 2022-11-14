fb0ab6b5c7b47b183220cacf961312dd064bc340
orion-junkins <orionjunkins@gmail.com>
2022-11-14 09:20:07 -0800

    Tests updated with --runslow flag introduced for API/S3 hits

--- tests/forecasting/data_fetching_utilities/weather_provider/open_meteo/test_open_meteo_adapter.py
+++ tests/forecasting/data_fetching_utilities/weather_provider/open_meteo/test_open_meteo_adapter.py
@@ -1,16 +1,22 @@
 import pytest
+from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
 
 from rlf.forecasting.data_fetching_utilities.weather_provider.open_meteo.open_meteo_adapter import OpenMeteoAdapter
 
 
 @pytest.fixture
 def fake_latitude() -> float:
-    return 1.0
+    return 30.0
 
 
 @pytest.fixture
 def fake_longitude() -> float:
-    return 2.0
+    return 30.0
+
+
+@pytest.fixture
+def fake_coordinate(fake_longitude, fake_latitude) -> Coordinate:
+    return Coordinate(lon=fake_longitude, lat=fake_latitude)
 
 
 @pytest.fixture
@@ -24,45 +30,43 @@ def fake_end_date() -> str:
 
 
 @pytest.fixture
-def weather_api_parameters(fake_latitude, fake_longitude, fake_start_date, fake_end_date) -> OpenMeteoAdapter:
-    return OpenMeteoAdapter(longitude=fake_longitude, latitude=fake_latitude, start_date=fake_start_date, end_date=fake_end_date)
+def weather_api_parameters() -> OpenMeteoAdapter:
+    return OpenMeteoAdapter()
 
 
-def test_get_returns_response_with_api_data():
-    adapter = OpenMeteoAdapter(
-        longitude=30.0, latitude=30.0, start_date="2022-08-14", end_date="2022-08-14")
-    response = adapter.get()
+@pytest.mark.slow
+def test_get_historical_returns_response_with_api_data(fake_coordinate, fake_start_date, fake_end_date):
+    adapter = OpenMeteoAdapter()
+    response = adapter.get_historical(coordinate=fake_coordinate, start_date=fake_start_date, end_date=fake_end_date)
     assert response.status_code == 200
     assert response.data["longitude"] == 30.0
     assert response.data["latitude"] == 30.0
-    assert response.data["elevation"] == 162.0
     assert response.data["hourly"]["temperature_2m"][0] == 23.6
 
 
-def test_get_location_returns_tuple(weather_api_parameters):
-    assert isinstance(weather_api_parameters.get_location(), tuple)
+@pytest.mark.slow
+def test_get_historical_columns_subset(fake_coordinate, fake_start_date, fake_end_date):
+    adapter = OpenMeteoAdapter()
+    response = adapter.get_historical(coordinate=fake_coordinate, start_date=fake_start_date, end_date=fake_end_date, columns=["temperature_2m"])
+    assert response.status_code == 200
+    assert len(response.data["hourly"]) == 2  # Time column and 'temperature_2m' column
+    assert len(response.data["hourly_units"]) == 2
 
 
-def test_get_location_returns_location(fake_latitude, fake_longitude, weather_api_parameters):
-    weather_api_parameters.set_location(fake_latitude, fake_longitude)
-    assert (weather_api_parameters.get_location()
-            == (fake_latitude, fake_longitude))
+@pytest.mark.slow
+def test_get_current_returns_response_with_api_data(fake_coordinate):
+    adapter = OpenMeteoAdapter()
+    response = adapter.get_current(coordinate=fake_coordinate, past_days=1, forecast_days=1)
+    assert response.status_code == 200
+    assert response.data["longitude"] == 30.0
+    assert response.data["latitude"] == 30.0
+    assert len(response.data['hourly']["temperature_2m"]) == 48  # 2 days of hourly data
 
 
-def test_set_start_date(fake_start_date, weather_api_parameters):
-    weather_api_parameters.set_start_date(fake_start_date)
-    assert (weather_api_parameters.get_start_date() == fake_start_date)
-
-
-def test_get_start_date(weather_api_parameters, fake_start_date):
-    assert (weather_api_parameters.get_start_date() == fake_start_date)
-
-
-def test_set_end_date(fake_end_date, weather_api_parameters):
-    weather_api_parameters.set_end_date(fake_end_date)
-    assert (weather_api_parameters.get_end_date() == fake_end_date)
-
-
-def test_get_end_date(fake_end_date, weather_api_parameters):
-    weather_api_parameters.end_date = fake_end_date
-    assert (weather_api_parameters.get_end_date() == fake_end_date)
+@pytest.mark.slow
+def test_get_current_columns_subset(fake_coordinate):
+    adapter = OpenMeteoAdapter()
+    response = adapter.get_current(coordinate=fake_coordinate, past_days=1, forecast_days=1, columns=["temperature_2m"])
+    assert response.status_code == 200
+    assert len(response.data["hourly"]) == 2  # Time column and 'temperature_2m' column
+    assert len(response.data["hourly_units"]) == 2
--- tests/forecasting/data_fetching_utilities/weather_provider/test_weather_provider.py
+++ tests/forecasting/data_fetching_utilities/weather_provider/test_weather_provider.py
@@ -1,48 +1,47 @@
+import pandas as pd
 import pytest
 
 from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
 from rlf.forecasting.data_fetching_utilities.weather_provider.api.base_api_adapter import BaseAPIAdapter
-from rlf.forecasting.data_fetching_utilities.weather_provider.weather_datum import WeatherDatum
 from rlf.forecasting.data_fetching_utilities.weather_provider.api.models import Response
 from rlf.forecasting.data_fetching_utilities.weather_provider.weather_provider import WeatherProvider
+from rlf.aws_dispatcher import AWSDispatcher
+
+
+def fake_response(coordinate):
+    response = Response(status_code=0,
+                        url="fake url",
+                        message="fake message",
+                        headers={"fake": "headers"},
+                        data={'latitude': coordinate.lat,
+                                'longitude': coordinate.lon,
+                                'generationtime_ms': 0.1,
+                                'utc_offset_seconds': 0,
+                                'timezone': 'GMT',
+                                'timezone_abbreviation': 'GMT',
+                                'elevation': 123.4,
+                                'hourly_units': {'time': 'iso8601', 'temperature_2m': '°C'},
+                                'hourly': {'time': ['2000-01-01T00:00',
+                                                    '2000-01-01T01:00',
+                                                    '2000-01-01T02:00'],
+                                            'temperature_2m': [1.0, 2.0, 3.0]}})
+    return response
 
 
 class FakeWeatherAPIAdapter(BaseAPIAdapter):
 
-    def __init__(self,
-                 longitude: float,
-                 latitude: float,
-                 start_date: str,
-                 end_date: str,
-                 protocol: str = "fake protocol",
-                 hostname: str = "fake hostname",
-                 version: str = "fake version",
-                 path: str = "fake path",
-                 hourly_parameters: list[str] = ["fake weather variable"]) -> None:
-        self.longitude = longitude
-        self.latitude = latitude
-        self.start_date = start_date
-        self.end_date = end_date
-        self.protocol = protocol
-        self.hostname = hostname
-        self.version = version
-        self.path = path
-        self.hourly_parameters = hourly_parameters
+    def get_current(self, coordinate: Coordinate, **kwargs) -> Response:
+        response = fake_response(coordinate=coordinate)
+        return response
 
-    def get(self) -> Response:
-        response = Response(status_code=0,
-                            url="fake url",
-                            message="fake message",
-                            headers={"fake": "headers"},
-                            data={"longitude": self.longitude,
-                                  "latitude": self.latitude,
-                                  "hourly": self.hourly_parameters})
+    def get_historical(self, coordinate: Coordinate, **kwargs) -> Response:
+        response = fake_response(coordinate=coordinate)
         return response
 
 
 @pytest.fixture
 def fake_weather_api_adapter() -> FakeWeatherAPIAdapter:
-    return FakeWeatherAPIAdapter(longitude=0.0, latitude=0.0, start_date="fake start", end_date="fake end")
+    return FakeWeatherAPIAdapter()
 
 
 @ pytest.fixture
@@ -54,25 +53,61 @@ def weather_provider(fake_weather_api_adapter) -> WeatherProvider:
     return weather_provider
 
 
-def test_fetch_historical_input_output_equal(weather_provider):
-    weather_datums = weather_provider.fetch_historical_weather_datums(
-        start_date="fake start", end_date="fake end")
-    assert len(weather_datums) == 2
+def test_fetch_historical_datums_fetches_one_per_location(weather_provider):
+    weather_datums = weather_provider.fetch_historical_datums()
+    assert len(weather_datums) == len(weather_provider.coordinates)
 
 
-def test_fetch_historical_returns_weather_datum(weather_provider):
-    weather_datums = weather_provider.fetch_historical_weather_datums(
-        start_date="fake start", end_date="fake end")
-    assert isinstance(weather_datums[0], WeatherDatum)
+def test_fetch_historical_returns_expected_df(weather_provider):
+    weather_dfs = weather_provider.fetch_historical()
+    for weather_df in weather_dfs:
+        assert isinstance(weather_df, pd.DataFrame)
+        assert list(weather_df.columns) == ["time", "temperature_2m"]
+        assert len(weather_df) == 3
 
 
-def test_fetch_historical_returns_datum_at_input_location(weather_provider):
-    weather_datums = weather_provider.fetch_historical_weather_datums(
-        start_date="fake start", end_date="fake end")
-    assert weather_datums[0].longitude == 1.0
-    assert weather_datums[0].latitude == 2.0
-    assert weather_datums[0].hourly_parameters == ["fake weather variable"]
+def test_fetch_current_datums_fetches_one_per_location(weather_provider):
+    weather_datums = weather_provider.fetch_current_datums()
+    assert len(weather_datums) == len(weather_provider.coordinates)
 
-    assert weather_datums[1].longitude == 3.0
-    assert weather_datums[1].latitude == 4.0
-    assert weather_datums[1].hourly_parameters == ["fake weather variable"]
+
+def test_fetch_current_returns_expected_df(weather_provider):
+    weather_dfs = weather_provider.fetch_current()
+    for weather_df in weather_dfs:
+        assert isinstance(weather_df, pd.DataFrame)
+        assert list(weather_df.columns) == ["time", "temperature_2m"]
+        assert len(weather_df) == 3
+
+
+# AWS Backed tests - TODO: Flag as slow
+@pytest.fixture
+def aws_dispatcher():
+    return AWSDispatcher(bucket_name="testing-bucket-junkinso", directory_name="weather_provider_testing")
+
+
+@ pytest.fixture
+def aws_weather_provider(fake_weather_api_adapter, aws_dispatcher) -> WeatherProvider:
+    coordinates = [Coordinate(lon=1.0, lat=2.0),
+                   Coordinate(lon=3.0, lat=4.0)]
+    weather_provider = WeatherProvider(
+        coordinates=coordinates, api_adapter=fake_weather_api_adapter, aws_dispatcher=aws_dispatcher)
+    return weather_provider
+
+
+@pytest.mark.slow
+def test_historical_data_aws(aws_weather_provider):
+    aws_weather_provider.update_historical_datums_in_aws()
+    datums = aws_weather_provider.download_historical_datums_from_aws()
+    for datum in datums:
+        assert isinstance(datum.hourly_parameters, pd.DataFrame)
+        assert list(datum.hourly_parameters.columns) == ["time", "temperature_2m"]
+        assert len(datum.hourly_parameters) == 3
+
+
+@pytest.mark.slow
+def test_fetch_historical_aws(aws_weather_provider):
+    weather_dfs = aws_weather_provider.fetch_historical()
+    for weather_df in weather_dfs:
+        assert isinstance(weather_df, pd.DataFrame)
+        assert list(weather_df.columns) == ["time", "temperature_2m"]
+        assert len(weather_df) == 3
