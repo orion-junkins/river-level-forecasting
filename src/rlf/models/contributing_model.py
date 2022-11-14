9a7befcbfa42bd3deb0cfe0bb21a23fea7c07910
Doug Dennis <douglasdennisjr@gmail.com>
2022-11-13 23:06:49 -0800

    i hate you, vscode

--- src/rlf/models/contributing_model.py
+++ src/rlf/models/contributing_model.py
@@ -15,14 +15,14 @@ class ContributingModel(GlobalForecastingModel):
         column_prefix: Optional[str] = None,
     ) -> None:
         """Initialize ContributingModel class.
-        
+
         Args:
             base_model (GlobalForecastingModel): Base Darts model that this tributary model wraps around.
             column_prefix (str, optional): If given then only columns in past_covariates or future_covariates that start with column_prefix will be passed to the base model. If None then all columns will be passed to the base model. Defaults to None.
         """
         self._base_model = base_model
         self._column_prefix = column_prefix
-    
+
     def fit(
         self,
         *,
@@ -32,19 +32,19 @@ class ContributingModel(GlobalForecastingModel):
         **kwargs,
     ) -> GlobalForecastingModel:
         """Fit the base model to given series and covariates.
-        
+
         Args:
             series (TimeSeries): Target data to use for fitting.
             past_covariates (CovariateType): Past covariates to use for training. See Darts documentation for information regarding this parameter.
             future_covariates (CovariateType): Future covariates to use for training. See Darts documentation for information regarding this parameter.
-        
+
         Returns:
             GlobalForecastingModel: self
         """
         past_covariates, future_covariates = self._preprocess_input_data(past_covariates, future_covariates)
         self._base_model.fit(series=series, past_covariates=past_covariates, future_covariates=future_covariates, **kwargs)
         return self
-    
+
     def predict(
         self,
         n: int,
@@ -54,19 +54,19 @@ class ContributingModel(GlobalForecastingModel):
         **kwargs,
     ) -> TimeSeries:
         """Predict n points using the supplied data.
-        
+
         Args:
             n (int): Number of points to predict.
             series (CovariateType): Series data to pass to base model. See Darts documentation for information regarding this parameter.
             past_covariates (CovariateType): Past covariates to use for prediction. See Darts documentation for information regarding this parameter.
             future_covariates (CovariateType): Future covariates to use for prediction. See Darts documentation for information regarding this parameter.
-        
+
         Returns:
             TimeSeries: n predictions of river levels.
         """
         past_covariates, future_covariates = self._preprocess_input_data(past_covariates, future_covariates)
         return self._base_model.predict(n=n, series=series, past_covariates=past_covariates, future_covariates=future_covariates, **kwargs)
-    
+
     @property
     def _fit_called(self) -> bool:
         return self._base_model._fit_called
@@ -77,11 +77,11 @@ class ContributingModel(GlobalForecastingModel):
         future_covariates: CovariateType = None
     ) -> tuple[CovariateType, CovariateType]:
         """If a column prefix is being used then return a filtered set of columns otherwise return the covariates.
-        
+
         Args:
             past_covariates (CovariateType): Covariates to filter.
             future_covariates (CovariateType): Covariates to filter.
-        
+
         Returns:
             CovariateType, CovariateType: Filtered past_covariates and future_covariates in that order.
         """
@@ -89,7 +89,6 @@ class ContributingModel(GlobalForecastingModel):
             past_covariates = self._modify_covariates(past_covariates)
             future_covariates = self._modify_covariates(future_covariates)
         return past_covariates, future_covariates
-
 
     def _modify_covariates(self, covariates: TimeSeries | Sequence[TimeSeries] | None) -> TimeSeries | Sequence[TimeSeries] | None:
         if isinstance(covariates, TimeSeries):
@@ -104,7 +103,6 @@ class ContributingModel(GlobalForecastingModel):
 
     def _find_columns_to_keep(self, all_columns: list[str]) -> set[str]:
         return {c for c in all_columns if c.startswith(self._column_prefix)}
-    
+
     def _model_encoder_settings(self) -> tuple[int, int, bool, bool]:
         return self._base_model._model_encoder_settings()
-    
