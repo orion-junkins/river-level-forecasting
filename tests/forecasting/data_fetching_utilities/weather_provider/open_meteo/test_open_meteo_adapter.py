07ff80ea9a1991d0723cb1167d074f91e906d09f
orion-junkins <orionjunkins@gmail.com>
2022-11-18 16:41:30 -0800

    Excess tests removed

--- tests/forecasting/data_fetching_utilities/weather_provider/open_meteo/test_open_meteo_adapter.py
+++ tests/forecasting/data_fetching_utilities/weather_provider/open_meteo/test_open_meteo_adapter.py
@@ -21,7 +21,7 @@ def fake_coordinate(fake_longitude, fake_latitude) -> Coordinate:
 
 @pytest.fixture
 def fake_start_date() -> str:
-    return "2022-08-14"
+    return "2022-09-13"
 
 
 @pytest.fixture
@@ -35,32 +35,12 @@ def weather_api_parameters() -> OpenMeteoAdapter:
 
 
 @pytest.mark.slow
-def test_get_historical_returns_response_with_api_data(fake_coordinate, fake_start_date, fake_end_date):
-    adapter = OpenMeteoAdapter()
-    response = adapter.get_historical(coordinate=fake_coordinate, start_date=fake_start_date, end_date=fake_end_date)
-    assert response.status_code == 200
-    assert response.data["longitude"] == 30.0
-    assert response.data["latitude"] == 30.0
-    assert response.data["hourly"]["temperature_2m"][0] == 23.6
-
-
-@pytest.mark.slow
 def test_get_historical_columns_subset(fake_coordinate, fake_start_date, fake_end_date):
     adapter = OpenMeteoAdapter()
     response = adapter.get_historical(coordinate=fake_coordinate, start_date=fake_start_date, end_date=fake_end_date, columns=["temperature_2m"])
     assert response.status_code == 200
     assert len(response.data["hourly"]) == 2  # Time column and 'temperature_2m' column
     assert len(response.data["hourly_units"]) == 2
-
-
-@pytest.mark.slow
-def test_get_current_returns_response_with_api_data(fake_coordinate):
-    adapter = OpenMeteoAdapter()
-    response = adapter.get_current(coordinate=fake_coordinate, past_days=1, forecast_days=1)
-    assert response.status_code == 200
-    assert response.data["longitude"] == 30.0
-    assert response.data["latitude"] == 30.0
-    assert len(response.data['hourly']["temperature_2m"]) == 48  # 2 days of hourly data
 
 
 @pytest.mark.slow
