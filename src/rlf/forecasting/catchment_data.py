0a26d088e08d32e303545cb3b10e911fc2662776
Doug Dennis <douglasdennisjr@gmail.com>
2022-11-20 16:33:48 -0800

    fix style

--- src/rlf/forecasting/catchment_data.py
+++ src/rlf/forecasting/catchment_data.py
@@ -1,13 +1,19 @@
-import pandas as pd
+from pandas import DataFrame
 
-from rlf.forecasting.data_fetching_utilities.weather_provider.weather_provider import WeatherProvider
 from rlf.forecasting.data_fetching_utilities.level_provider.base_level_provider import BaseLevelProvider
+from rlf.forecasting.data_fetching_utilities.weather_provider.weather_provider import WeatherProvider
 
 
 class CatchmentData:
     """Abstraction for containing all data pertaining to a single catchment. Acts as a cache for fetched data with the ability to update/refetch when desired. Data is not fetched until first access. Thus, this class may be used in production environments for inference without loading excess historical data."""
 
-    def __init__(self, catchment_name: str, weather_provider: WeatherProvider, level_provider: BaseLevelProvider, num_recent_samples: int = 90*24) -> None:
+    def __init__(
+        self,
+        catchment_name: str,
+        weather_provider: WeatherProvider,
+        level_provider: BaseLevelProvider,
+        num_recent_samples: int = 90*24
+    ) -> None:
         """
         Create a CatchmentData instance.
 
@@ -15,15 +21,15 @@ class CatchmentData:
             catchment_name (str): Name of the catchment. Should correspond to external gauge name.
             weather_provider (WeatherProvider): Provider for Weather data.
             level_provider (LevelProvider): Provider for level data.
-            num_recent_samples (_type_, optional): Number of recent level samples to fetch. Defaults to 90 days (90 days * 24 hours/day).
+            num_recent_samples (int, optional): Number of recent level samples to fetch. Defaults to 90 days (90 days * 24 hours/day).
         """
         self.name = catchment_name
         self.weather_provider = weather_provider
         self.level_provider = level_provider
         self.num_recent_samples = num_recent_samples
 
-        self._all_current = (None, None)        # (weather_data, level_data)
-        self._all_historical = (None, None)     # (weather_data, level_data)
+        self._all_current = (None, None)  # (weather_data, level_data)
+        self._all_historical = (None, None)  # (weather_data, level_data)
 
     @property
     def num_weather_datasets(self) -> int:
@@ -42,11 +48,11 @@ class CatchmentData:
         return len(current_weather)
 
     @property
-    def all_current(self) -> tuple[list[pd.DataFrame], pd.DataFrame]:
+    def all_current(self) -> tuple[list[DataFrame], DataFrame]:
         """All current data. A tuple containing a list of current weather data with one DataFrame per location, and a single DataFrame of level data.
 
         Returns:
-            tuple[list[pd.DataFrame], pd.DataFrame]: All current data for the Catchment.
+            tuple[list[DataFrame], DataFrame]: All current data for the Catchment.
         """
         if self._all_current == (None, None):
             self._fetch_all_current()
@@ -54,23 +60,22 @@ class CatchmentData:
         return self._all_current
 
     def _fetch_all_current(self) -> None:
-        """Fetch or refetch all current data, updating the member variable _all_current. This will trigger queries to the underlying weather and level providers.
-        """
+        """Fetch or refetch all current data, updating the member variable _all_current. This will trigger queries to the underlying weather and level providers."""
         current_weather = self.weather_provider.fetch_current()
         recent_level = self.level_provider.fetch_recent_level(self.num_recent_samples)
 
         self._all_current = (current_weather, recent_level)
 
-    def update_for_inference(self):
+    def update_for_inference(self) -> None:
         """External facing helping to update current data for inference."""
         self._fetch_all_current()
 
     @property
-    def all_historical(self) -> tuple[list[pd.DataFrame], pd.DataFrame]:
+    def all_historical(self) -> tuple[list[DataFrame], DataFrame]:
         """All historical data. A tuple containing a list of historical weather data with one DataFrame per location, and a single DataFrame of level data.
 
         Returns:
-            tuple[list[pd.DataFrame], pd.DataFrame]: All historical data for the Catchment.
+            tuple[list[DataFrame], DataFrame]: All historical data for the Catchment.
         """
         if self._all_historical == (None, None):
             self._fetch_all_historical()
@@ -78,8 +83,7 @@ class CatchmentData:
         return self._all_historical
 
     def _fetch_all_historical(self) -> None:
-        """Fetch or refetch all current data, updating the member variable _all_current. This will trigger queries to the underlying weather and level providers.
-        """
+        """Fetch or refetch all current data, updating the member variable _all_current. This will trigger queries to the underlying weather and level providers."""
         historical_weather = self.weather_provider.fetch_historical()
         historical_level = self.level_provider.fetch_historical_level()
         self._all_historical = (historical_weather, historical_level)
