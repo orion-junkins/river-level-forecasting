10c142ac9e2d1a8c6873ee452900f4f215591698
orion-junkins <orionjunkins@gmail.com>
2022-12-01 14:48:56 -0800

    Rest Invoker timeout increased to 5 seconds (fixes #46) (#47)

--- src/rlf/forecasting/data_fetching_utilities/weather_provider/api/rest_invoker.py
+++ src/rlf/forecasting/data_fetching_utilities/weather_provider/api/rest_invoker.py
@@ -52,7 +52,7 @@ class RestInvoker():
                 verify=self._ssl_verify,
                 params=parameters,
                 json=data,
-                timeout=1,
+                timeout=5,
                 headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.52"})
             if response.status_code == 200:
                 return Response(status_code=response.status_code, url=response.url, message=response.reason, headers=response.headers, data=response.json())
