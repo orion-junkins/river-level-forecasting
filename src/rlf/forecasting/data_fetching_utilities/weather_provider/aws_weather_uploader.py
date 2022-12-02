76994064d81f11b8baa4e7ccd9eaf6ccabfa3a8d
orion-junkins <orionjunkins@gmail.com>
2022-12-01 16:16:36 -0800

    AWSUploader created

---
+++ src/rlf/forecasting/data_fetching_utilities/weather_provider/aws_weather_uploader.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from typing import Optional
+
+import pandas as pd
+
+from rlf.aws_dispatcher import AWSDispatcher
+from rlf.forecasting.data_fetching_utilities.weather_provider.base_weather_provider import (
+    BaseWeatherProvider
+)
+
+DEFAULT_START_DATE = "2022-01-01"
+DEFAULT_END_DATE = datetime.now().strftime("%Y-%m-%d")
+
+
+class AWSWeatherUploader():
+    """Utility for fetching and storing data from some WeatherProvider into AWS. Generally used to make data accessible to AWSWeatherProvider instances"""
+
+    def __init__(self,
+                 weather_provider: BaseWeatherProvider,
+                 aws_dispatcher: AWSDispatcher) -> None:
+        """
+        Create a new AWSWeatherUploader instance.
+
+        Args:
+            weather_provider (BaseWeatherProvider): Source for fetching data.
+            aws_dispatcher (AWSDispatcher): An AWSDispatcher to upload data to.
+        """
+        self.weather_provider = weather_provider
+        self.aws_dispatcher = aws_dispatcher
+
+    def upload_historical(self,
+                          start_date: str = DEFAULT_START_DATE,
+                          end_date: str = DEFAULT_END_DATE,
+                          columns: Optional[list[str]] = None,
+                          years_per_query=2,
+                          sleep_duration=0) -> None:
+        """Refetch historical datums and store this updated data in AWS. This will overwrite whatever data was previously stored for the current river.
+
+        Args:
+            start_date (str, optional): iso8601 format YYYY-MM-DD. Defaults to DEFAULT_START_DATE.
+            end_date (str, optional): iso8601 format YYYY-MM-DD. Defaults to DEFAULT_END_DATE.
+            columns (list[str], optional): The columns/parameters to fetch. All available will be fetched if left equal to None. Defaults to None.
+            years_per_query (int, optional): How many years to fetch in a single query. Defaults to 2.
+            sleep_duration (int, optional): How long to sleep after each query. Helps prevent throttling. Defaults to 0.
+        """
+        start_date = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date = datetime.strptime(end_date, "%Y-%m-%d")
+
+        partition_end_date = min(
+            [start_date.replace(start_date.year + years_per_query), end_date])
+
+        datums = self.weather_provider.fetch_historical(
+            start_date=datetime.strftime(start_date, "%Y-%m-%d"),
+            end_date=datetime.strftime(partition_end_date, "%Y-%m-%d"),
+            columns=columns,
+            sleep_duration=sleep_duration)
+
+        start_date = start_date.replace(start_date.year + years_per_query)
+        partition_end_date = partition_end_date.replace(
+            partition_end_date.year + years_per_query)
+
+        while partition_end_date < end_date:
+            partial_datums = self.weather_provider.fetch_historical(
+                start_date=datetime.strftime(start_date, "%Y-%m-%d"),
+                end_date=datetime.strftime(partition_end_date, "%Y-%m-%d"),
+                columns=columns,
+                sleep_duration=sleep_duration)
+
+            for (datum, partial_datum) in zip(datums, partial_datums):
+                partial_hourly_parameters = partial_datum.hourly_parameters
+                datum.hourly_parameters = pd.concat([datum.hourly_parameters,
+                                                    partial_hourly_parameters])
+
+            start_date = start_date.replace(start_date.year + years_per_query)
+            partition_end_date = partition_end_date.replace(
+                partition_end_date.year + years_per_query)
+
+        for datum in datums:
+            self.aws_dispatcher.upload_datum(datum)
---
+++ tests/forecasting/data_fetching_utilities/weather_provider/test_aws_weather_uploader.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import pytest
+
+from rlf.aws_dispatcher import AWSDispatcher
+from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
+from rlf.forecasting.data_fetching_utilities.weather_provider.aws_weather_provider import AWSWeatherProvider
+from rlf.forecasting.data_fetching_utilities.weather_provider.api_weather_provider import APIWeatherProvider
+from rlf.forecasting.data_fetching_utilities.weather_provider.aws_weather_uploader import AWSWeatherUploader
+
+DEFAULT_START_DATE = "2020-01-01"
+END_DATE = "2020-02-01"
+
+
+@pytest.fixture
+def coordinates() -> list[Coordinate]:
+    return [Coordinate(lon=1.0, lat=2.0), Coordinate(lon=3.0, lat=4.0)]
+
+
+@pytest.fixture
+def api_weather_provider(coordinates):
+    return APIWeatherProvider(coordinates=coordinates)
+
+
+@pytest.fixture
+def aws_dispatcher() -> AWSDispatcher:
+    return AWSDispatcher("historical-weather", "testing")
+
+
+@pytest.fixture
+def aws_weather_uploader(api_weather_provider, aws_dispatcher):
+    return AWSWeatherUploader(weather_provider=api_weather_provider, aws_dispatcher=aws_dispatcher)
+
+
+@ pytest.fixture
+def weather_provider(coordinates, aws_dispatcher) -> AWSWeatherProvider:
+    weather_provider = AWSWeatherProvider(
+        coordinates=coordinates, aws_dispatcher=aws_dispatcher)
+    return weather_provider
+
+
+def test_upload_historical(aws_weather_uploader, weather_provider):
+    aws_weather_uploader.upload_historical(start_date=DEFAULT_START_DATE, end_date=END_DATE)
+    weather_datums = weather_provider.fetch_historical()
+    assert len(weather_datums) == len(weather_provider.coordinates)
+
+    expected_start_date = pd.to_datetime(DEFAULT_START_DATE, format='%Y-%m-%d')
+    expected_end_date = pd.to_datetime(END_DATE, format='%Y-%m-%d')
+
+    for weather_datum in weather_datums:
+        actual_start_date = weather_datum.hourly_parameters.index[0]
+        actual_end_date = weather_datum.hourly_parameters.index[-1]
+
+        assert (expected_start_date.year == actual_start_date.year)
+        assert (expected_start_date.month == actual_start_date.month)
+        assert (expected_start_date.day == actual_start_date.day)
+
+        assert (expected_end_date.year == actual_end_date.year)
+        assert (expected_end_date.month == actual_end_date.month)
+        assert (expected_end_date.day == actual_end_date.day)
