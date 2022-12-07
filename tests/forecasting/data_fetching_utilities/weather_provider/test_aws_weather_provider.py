bf3072f1d33a55cde8f9264e204fcab53d3c699f
orion-junkins <orionjunkins@gmail.com>
2022-12-07 15:16:00 -0800

    Upload and fetch tests added for current weather data

--- tests/forecasting/data_fetching_utilities/weather_provider/test_aws_weather_provider.py
+++ tests/forecasting/data_fetching_utilities/weather_provider/test_aws_weather_provider.py
@@ -21,8 +21,19 @@ def weather_provider(aws_dispatcher, coordinates) -> AWSWeatherProvider:
         coordinates=coordinates, aws_dispatcher=aws_dispatcher)
 
 
+@pytest.mark.slow
 def test_fetch_historical(weather_provider):
     weather_datums = weather_provider.fetch_historical()
+    assert len(weather_datums) == len(weather_provider.coordinates)
+    for weather_datum in weather_datums:
+        assert weather_datum.hourly_parameters.index.dtype == "datetime64[ns, UTC]"
+        assert ("temperature_2m" in list(weather_datum.hourly_parameters.columns))
+
+
+@pytest.mark.slow
+def test_fetch_current(weather_provider):
+    weather_provider.set_timestamp("22-12-07_15-14")
+    weather_datums = weather_provider.fetch_current()
     assert len(weather_datums) == len(weather_provider.coordinates)
     for weather_datum in weather_datums:
         assert weather_datum.hourly_parameters.index.dtype == "datetime64[ns, UTC]"
--- tests/forecasting/data_fetching_utilities/weather_provider/test_aws_weather_uploader.py
+++ tests/forecasting/data_fetching_utilities/weather_provider/test_aws_weather_uploader.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import pandas as pd
 import pytest
 
@@ -9,6 +10,7 @@ from rlf.forecasting.data_fetching_utilities.weather_provider.aws_weather_upload
 
 DEFAULT_START_DATE = "2020-01-01"
 END_DATE = "2020-02-01"
+CURRENT_TIMESTAMP = "22-12-04_09-25"
 
 
 @pytest.fixture
@@ -38,6 +40,7 @@ def weather_provider(coordinates, aws_dispatcher) -> AWSWeatherProvider:
     return weather_provider
 
 
+@pytest.mark.slow
 def test_upload_historical(aws_weather_uploader, weather_provider):
     aws_weather_uploader.upload_historical(start_date=DEFAULT_START_DATE, end_date=END_DATE)
     weather_datums = weather_provider.fetch_historical()
@@ -45,6 +48,34 @@ def test_upload_historical(aws_weather_uploader, weather_provider):
 
     expected_start_date = pd.to_datetime(DEFAULT_START_DATE, format='%Y-%m-%d')
     expected_end_date = pd.to_datetime(END_DATE, format='%Y-%m-%d')
+
+    for weather_datum in weather_datums:
+        actual_start_date = weather_datum.hourly_parameters.index[0]
+        actual_end_date = weather_datum.hourly_parameters.index[-1]
+
+        assert (expected_start_date.year == actual_start_date.year)
+        assert (expected_start_date.month == actual_start_date.month)
+        assert (expected_start_date.day == actual_start_date.day)
+
+        assert (expected_end_date.year == actual_end_date.year)
+        assert (expected_end_date.month == actual_end_date.month)
+        assert (expected_end_date.day == actual_end_date.day)
+
+
+@pytest.mark.slow
+def test_upload_current(aws_weather_uploader, weather_provider):
+    # Generate timestamp string for directory naming
+    now = datetime.now()
+    timestamp = now.strftime("%y-%m-%d_%H-%M")
+    dir_path = str(timestamp)
+
+    expected_start_date = now - timedelta(days=92)
+    expected_end_date = now + timedelta(days=15)
+
+    aws_weather_uploader.upload_current(dir_path=dir_path)
+    weather_provider.set_timestamp(dir_path)
+    weather_datums = weather_provider.fetch_current()
+    assert len(weather_datums) == len(weather_provider.coordinates)
 
     for weather_datum in weather_datums:
         actual_start_date = weather_datum.hourly_parameters.index[0]
