6bcd3a23cc61666fead73cf00731590cec08c537
orion-junkins <orionjunkins@gmail.com>
2023-01-13 12:44:18 -0800

    Historical upload script updated to match current upload script

--- scripts/upload/weather/historical_meteo_to_aws.py
+++ scripts/upload/weather/historical_meteo_to_aws.py
@@ -1,24 +1,35 @@
 # Helper script for uploading historical weather from OpenMeteo to AWS. Intended to be modified & run as needed to upload new historical datasets.
-# %%
+import json
+
 from rlf.aws_dispatcher import AWSDispatcher
 from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
 from rlf.forecasting.data_fetching_utilities.weather_provider.api_weather_provider import APIWeatherProvider
 from rlf.forecasting.data_fetching_utilities.weather_provider.aws_weather_uploader import AWSWeatherUploader
 
 # Tunable parameters
-coordinates = [Coordinate(lon=-120.75, lat=44.25), Coordinate(lon=-121.0, lat=44.5)]
-start_date = "2010-01-01"
-end_date = "2021-01-01"
-sleep_duration = 5
-bucket_name = "all-weather-data"
-directory_name = "open-meteo"
+CATCHMENT_FILEPATH = "path/to/catchment/file.json"
+START_DATE = "2010-01-01"
+END_DATE = "2021-01-01"
+SLEEP_DURATION = 5
+BUCKET_NAME = "all-weather-data"
+AWS_DIR_NAME = "open-meteo"
 
+# Load a list of Coordinate objects from the json file at the given path
+with open(CATCHMENT_FILEPATH) as f:
+    data = json.load(f)
+coordinates_raw = []
+for feature in data["features"]:
+    coordinates_raw.extend(feature["geometry"]["coordinates"])
+coordinates = []
+for coord in coordinates_raw:
+    new_coord = Coordinate(lon=coord[0], lat=coord[1])
+    coordinates.append(new_coord)
+coordinates = list(set(coordinates))
+
+# Instantiate an APIWeatherProvider, AWSDispatcher, and AWSWeatherUploader
 api_weather_provider = APIWeatherProvider(coordinates=coordinates)
-
-aws_dispatcher = AWSDispatcher(bucket_name=bucket_name, directory_name=directory_name)
-
+aws_dispatcher = AWSDispatcher(bucket_name=BUCKET_NAME, directory_name=AWS_DIR_NAME)
 aws_weather_uploader = AWSWeatherUploader(weather_provider=api_weather_provider, aws_dispatcher=aws_dispatcher)
 
-aws_weather_uploader.upload_historical(start_date=start_date, end_date=end_date, sleep_duration=sleep_duration)
-
-# %%
+# Upload historical weather to AWS
+aws_weather_uploader.upload_historical(start_date=START_DATE, end_date=END_DATE, sleep_duration=SLEEP_DURATION)
