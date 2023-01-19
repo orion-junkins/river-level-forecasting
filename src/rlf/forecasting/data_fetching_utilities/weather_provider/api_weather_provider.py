55ea59a101997ba3fed304569029229424780cbb
Zach Bochanski <ztbochanski@gmail.com>
2023-01-18 20:12:16 -0800

    update documentation build_datum_from_response change default to .1 in docs

--- src/rlf/forecasting/data_fetching_utilities/weather_provider/api_weather_provider.py
+++ src/rlf/forecasting/data_fetching_utilities/weather_provider/api_weather_provider.py
@@ -44,7 +44,8 @@ class APIWeatherProvider(BaseWeatherProvider):
     def _build_hourly_parameters_from_response(self, hourly_parameters_response: dict, tz: str) -> DataFrame:
         index_parameter = self.api_adapter.get_index_parameter()
         df = DataFrame(hourly_parameters_response)
-        df.index = df[index_parameter].map(lambda x: datetime.fromisoformat(x).replace(tzinfo=pytz.timezone(tz)).astimezone(pytz.timezone("UTC")))
+        df.index = df[index_parameter].map(lambda x: datetime.fromisoformat(
+            x).replace(tzinfo=pytz.timezone(tz)).astimezone(pytz.timezone("UTC")))
         df.drop(columns=[index_parameter], inplace=True)
         return df
 
@@ -53,7 +54,7 @@ class APIWeatherProvider(BaseWeatherProvider):
 
         Args:
             response (Response): The Response to draw data from.
-            precision (float): The precision for rounding lat and lon. Defaults to 0.25.
+            precision (float): The precision for rounding lat and lon. Defaults to 0.1.
 
         Returns:
             WeatherDatum: The constructed WeatherDatum instance.
@@ -95,7 +96,8 @@ class APIWeatherProvider(BaseWeatherProvider):
         Returns:
             WeatherDatum: A Datum object containing the weather data and metadata about a coordinate.
         """
-        response = self.api_adapter.get_historical(coordinate=coordinate, start_date=start_date, end_date=end_date, columns=columns)
+        response = self.api_adapter.get_historical(
+            coordinate=coordinate, start_date=start_date, end_date=end_date, columns=columns)
 
         datum = self.build_datum_from_response(response)
 
@@ -137,7 +139,8 @@ class APIWeatherProvider(BaseWeatherProvider):
         Returns:
             WeatherDatum: A Datum object containing the weather data and metadata about a coordinate
         """
-        response = self.api_adapter.get_current(coordinate=coordinate, columns=columns)
+        response = self.api_adapter.get_current(
+            coordinate=coordinate, columns=columns)
 
         datum = self.build_datum_from_response(response)
 
