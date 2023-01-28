d1c5f4acacb1ea864cfb063899c80b2c9c5e8731
Zach Bochanski <ztbochanski@gmail.com>
2023-01-27 16:38:10 -0800

    make more consistent coords for testing

--- demos/demo_api_weather_provider_response_accuracy.py
+++ demos/demo_api_weather_provider_response_accuracy.py
@@ -4,15 +4,15 @@ from rlf.forecasting.data_fetching_utilities.weather_provider.api_weather_provid
 
 # Declare coordinates
 coordinates = [Coordinate(lon=-120.1, lat=44.1),
-               Coordinate(lon=-121.2, lat=44.2),
-               Coordinate(lon=-121.3, lat=44.3),
-               Coordinate(lon=-121.4, lat=45.4),
-               Coordinate(lon=-121.5, lat=45.5),
-               Coordinate(lon=-122.6, lat=45.6),
-               Coordinate(lon=-122.7, lat=45.7),
-               Coordinate(lon=-122.8, lat=46.8),
-               Coordinate(lon=-122.9, lat=46.9),
-               Coordinate(lon=-123.0, lat=47.0)]
+               Coordinate(lon=-120.2, lat=44.2),
+               Coordinate(lon=-120.3, lat=44.3),
+               Coordinate(lon=-120.4, lat=44.4),
+               Coordinate(lon=-120.5, lat=44.5),
+               Coordinate(lon=-120.6, lat=44.6),
+               Coordinate(lon=-120.7, lat=44.7),
+               Coordinate(lon=-120.8, lat=44.8),
+               Coordinate(lon=-120.9, lat=44.9),
+               Coordinate(lon=-121.0, lat=45.0)]
 
 # Create an AWSWeatherProvider using the defined coordinates
 weather_provider = APIWeatherProvider(coordinates)
