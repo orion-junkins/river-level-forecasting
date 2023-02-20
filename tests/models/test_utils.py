6d2fc9ad167f6c5ace938709625c6a4605d4e90b
Doug Dennis <douglasdennisjr@gmail.com>
2023-02-20 15:57:45 -0800

    save and load tests

--- tests/models/test_utils.py
+++ tests/models/test_utils.py
@@ -1,9 +1,11 @@
+import os
+
 from darts.models.forecasting.regression_ensemble_model import RegressionEnsembleModel
 from darts.models.forecasting.rnn_model import RNNModel
 from pytorch_lightning import Trainer
 
 from rlf.models.contributing_model import ContributingModel
-from rlf.models.utils import repair_regression_ensemble_model
+from rlf.models.utils import repair_regression_ensemble_model, save_ensemble_model, load_ensemble_model
 
 
 def test_repair_contributing_model():
@@ -22,3 +24,27 @@ def test_repair_regular_model():
     repair_regression_ensemble_model(ensemble_model)
 
     assert isinstance(ensemble_model.models[0].trainer, Trainer)
+
+
+def test_save_ensemble_model(tmp_path):
+    ensemble_model = RegressionEnsembleModel([ContributingModel(RNNModel(1))], 1)
+
+    save_ensemble_model(tmp_path, ensemble_model)
+
+    assert os.path.exists(f"{tmp_path}/ensemble")
+    assert os.path.exists(f"{tmp_path}/contributing_model_0")
+    assert os.path.exists(f"{tmp_path}/contributing_model_0_base_model")
+
+
+def test_load_ensemble_model(tmp_path):
+    ensemble_model = RegressionEnsembleModel([ContributingModel(RNNModel(1))], 1)
+    ensemble_model.__test_tag = "ensemble_model"
+    ensemble_model.models[0].__test_tag = "contributing_model"
+    ensemble_model.models[0]._base_model.__test_tag = "base_model"
+
+    save_ensemble_model(tmp_path, ensemble_model)
+    loaded_model = load_ensemble_model(tmp_path)
+
+    assert loaded_model.__test_tag == "ensemble_model"
+    assert loaded_model.models[0].__test_tag == "contributing_model"
+    assert loaded_model.models[0]._base_model.__test_tag == "base_model"
