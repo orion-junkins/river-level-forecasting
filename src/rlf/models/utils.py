da4650a26dcaed102be66e0f39cd7346aeab1130
Doug Dennis <douglasdennisjr@gmail.com>
2023-02-20 15:46:49 -0800

    add ensemble model load and save functions

--- src/rlf/models/utils.py
+++ src/rlf/models/utils.py
@@ -1,3 +1,5 @@
+import os
+
 from darts.models.forecasting.regression_ensemble_model import RegressionEnsembleModel
 from pytorch_lightning import Trainer
 
@@ -19,3 +21,41 @@ def repair_regression_ensemble_model(model: RegressionEnsembleModel) -> None:
             contributing_model._base_model.trainer = Trainer()
         else:
             contributing_model.trainer = Trainer()
+
+
+def save_ensemble_model(base_dir: str, model: RegressionEnsembleModel) -> None:
+    """Save an ensemble model to disk.
+
+    Args:
+        base_dir (str): Base directory to save the ensemble model and its contributing models to.
+        model (RegressionEnsembleModel): Ensemble model to save.
+    """
+    for i, contributing_model in enumerate(model.models):
+        contributing_model.save(os.path.join(base_dir, f"contributing_model_{i}"))
+
+    old_models = model.models
+    model.models = [None] * len(model.models)
+
+    model.save(os.path.join(base_dir, "ensemble"))
+
+    model.models = old_models
+
+
+def load_ensemble_model(base_dir: str) -> RegressionEnsembleModel:
+    """Load an ensemble model from disk.
+
+    Args:
+        path (str): Base directory where the ensemble model and its contributing models are located.
+
+    Returns:
+        RegressionEnsembleModel: Loaded ensemble model.
+    """
+    model = RegressionEnsembleModel.load(os.path.join(base_dir, "ensemble"))
+    contributing_models = [
+        ContributingModel.load(os.path.join(base_dir, f"contributing_model_{i}"))
+        for i in range(len(model.models))
+    ]
+
+    model.models = contributing_models
+
+    return model
