0e21d124b0da15d3a286bade8e5226741c025d49
Doug Dennis <douglasdennisjr@gmail.com>
2023-02-20 16:23:44 -0800

    fix inference forecaster tests

--- tests/forecasting/test_inference_forecaster.py
+++ tests/forecasting/test_inference_forecaster.py
@@ -7,6 +7,29 @@ from rlf.forecasting.inference_forecaster import InferenceForecaster
 from fake_providers import FakeLevelProvider, FakeWeatherProvider
 
 
+class FakeScaler:
+    def __init__(self, scale):
+        self._scale = scale
+
+    def transform(self, X):
+        return X * self._scale
+
+class FakeInferenceForecaster(InferenceForecaster):
+
+    def __init__(self, mock_model, *args, **kwargs):
+        self._mock_model = mock_model
+        super().__init__(*args, **kwargs)
+
+    def _load_ensemble(self):
+        return self._mock_model
+
+    def _load_scalers(self):
+        return {"scaler": FakeScaler(1.0), "target_scaler": FakeScaler(1.0)}
+
+    def _load_metadata(self) -> dict:
+        return {"api_columns": ["A", "B"]}
+
+
 @pytest.fixture
 def catchment_data():
     return CatchmentData("test_catchment", FakeWeatherProvider(num_historical_samples=1000), FakeLevelProvider(num_historical_samples=1000))
@@ -27,7 +50,7 @@ def inference_dataset(scalers, catchment_data):
     return InferenceDataset(catchment_data=catchment_data, scaler=scalers[0], target_scaler=scalers[1])
 
 
-def test_inference_forecaster_init(inference_dataset, catchment_data):
-    inference_forecaster = InferenceForecaster(catchment_data=catchment_data, dataset=inference_dataset)
+def test_inference_forecaster_init(catchment_data):
+    inference_forecaster = FakeInferenceForecaster(None, catchment_data=catchment_data)
 
     assert (type(inference_forecaster.dataset) is InferenceDataset)
