e0245aa1e38be75dafbebd651824bcf4bae30dcf
Doug Dennis <douglasdennisjr@gmail.com>
2023-02-23 22:34:22 -0800

    fixed up inference forecaster test

--- tests/forecasting/test_inference_forecaster.py
+++ tests/forecasting/test_inference_forecaster.py
@@ -16,10 +16,10 @@ class FakeObject:
 
 
 class MockModel:
-    def __init__(self, expected_n, expected_series, expected_past_covariates, prediction):
+    def __init__(self, expected_n, expected_series, expected_future_covariates, prediction):
         self.expected_n = expected_n
         self.expected_series = expected_series
-        self.expected_past_covariates = expected_past_covariates
+        self.expected_future_covariates = expected_future_covariates
         self.prediction = prediction
         self.models = [FakeObject({
             "_column_prefix": "prefix_",
@@ -30,13 +30,14 @@ class MockModel:
             })
         })]
 
-    def predict(self, n, series, past_covariates):
+    def predict(self, n, series, future_covariates):
+        print(series)
+        print(self.expected_series)
         assert n == self.expected_n
         assert series == self.expected_series
-        assert past_covariates == self.expected_past_covariates
+        assert future_covariates == self.expected_future_covariates
         return self.prediction
 
-
 class FakeScaler:
     def __init__(self, scale):
         self._scale = scale
@@ -46,35 +47,16 @@ class FakeScaler:
 
 class FakeInferenceForecaster(InferenceForecaster):
 
-    def __init__(self, mock_model, *args, **kwargs):
+    def __init__(self, mock_model, scalers, *args, **kwargs):
         self._mock_model = mock_model
-        super().__init__(*args, **kwargs)
-
-    def _load_ensemble(self):
-        return self._mock_model
-
-    def _load_scalers(self):
-        return {"scaler": FakeScaler(1.0), "target_scaler": FakeScaler(1.0)}
-
-
-class FakeScaler:
-    def __init__(self, scale):
-        self._scale = scale
-
-    def transform(self, X):
-        return X * self._scale
-
-class FakeInferenceForecaster(InferenceForecaster):
-
-    def __init__(self, mock_model, *args, **kwargs):
-        self._mock_model = mock_model
+        self._scalers = scalers
         super().__init__(*args, **kwargs)
 
     def _load_ensemble(self, load_cpu):
         return self._mock_model
 
     def _load_scalers(self):
-        return {"scaler": FakeScaler(1.0), "target_scaler": FakeScaler(1.0)}
+        return {"scaler": self._scalers[0], "target_scaler": self._scalers[1]}
 
     def _load_metadata(self) -> dict:
         return {
@@ -106,15 +88,15 @@ def inference_dataset(scalers, catchment_data):
     return InferenceDataset(catchment_data=catchment_data, scaler=scalers[0], target_scaler=scalers[1])
 
 
-def test_inference_forecaster_init(catchment_data):
-    inference_forecaster = FakeInferenceForecaster(None, catchment_data=catchment_data)
+def test_inference_forecaster_init(catchment_data, scalers):
+    inference_forecaster = FakeInferenceForecaster(None, scalers, catchment_data=catchment_data)
 
     assert (type(inference_forecaster.dataset) is InferenceDataset)
 
 
-def test_inference_forecaster_predict(inference_dataset, catchment_data):
+def test_inference_forecaster_predict(inference_dataset, catchment_data, scalers):
     mock_model = MockModel(24, inference_dataset.y, inference_dataset.X, [n for n in range(24)])
-    inference_forecaster = FakeInferenceForecaster(mock_model, catchment_data=catchment_data)
+    inference_forecaster = FakeInferenceForecaster(mock_model, scalers, catchment_data=catchment_data)
     actual_results = inference_forecaster.predict()
     expected_results = [n for n in range(24)]
 
