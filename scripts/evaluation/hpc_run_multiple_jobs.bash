d9fba5b0eb56e12e3f7df4071bc13cdc8f5b6370
Orion Junkins <junkinso@submit-a.hpc.engr.oregonstate.edu>
2023-04-27 11:03:32 -0700

    hpc scripts updated

--- scripts/evaluation/hpc_run_multiple_jobs.bash
+++ scripts/evaluation/hpc_run_multiple_jobs.bash
@@ -4,7 +4,6 @@ GAUGE=$2
 START=$3
 END=$4
  
-for i in {$START..$END}
-do
-    bash scripts/evaluation/hpc_run_single_job.bash $1 $2 $i
+for ((i = $START; i < $END; i++)); do
+    sbatch scripts/evaluation/hpc_run_single_job.bash $MODEL $GAUGE $i
 done
--- scripts/evaluation/hpc_run_single_job.bash
+++ scripts/evaluation/hpc_run_single_job.bash
@@ -6,6 +6,9 @@
 #SBATCH --nodelist=cn-m-1	#use server 1
 #SBATCH --mem=20G			#20GB RAM per node
 #SBATCH -t 4-00:00:00		#4 day timeout
+#SBATCH --mail-user=junkinso@oregonstate.edu
+#SBATCH --mail-type=END
 #SBATCH --export=ALL
-source activate river-level		#activate GPU env.
-python scripts/evaluation/run_grid_search_jobs.py $1 $2 $3 # RNN 14219000 0
+echo SLURM Running single GS job. Model: $1 Gauge: $2 Job: $3
+source activate river-level-2
+python scripts/evaluation/run_single_gs_job.py $1 $2 $3 # RNN 14219000 0
