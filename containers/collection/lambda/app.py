17c6ebd54c3efd7c97f239bd2f27ce8465275748
orion-junkins <orionjunkins@gmail.com>
2023-05-03 20:45:39 -0600

    Coordinate file parsing

--- containers/collection/lambda/app.py
+++ containers/collection/lambda/app.py
@@ -1,36 +1,30 @@
+import json
 from datetime import datetime
 
 from rlf.aws_dispatcher import AWSDispatcher
 from rlf.forecasting.data_fetching_utilities.coordinate import Coordinate
 from rlf.forecasting.data_fetching_utilities.weather_provider.api_weather_provider import APIWeatherProvider
 from rlf.forecasting.data_fetching_utilities.weather_provider.aws_weather_uploader import AWSWeatherUploader
-ALL_COORDINATES = list(
-    map(lambda x: Coordinate(x[0], x[1]),
-        [
-        [
-            -122.4,
-            45.8
-        ],
-        [
-            -122.3,
-            45.8
-        ],
-        [
-            -122.2,
-            45.8
-        ]
-    ]
-    ))
 
 
 def handler(event, context):
-    print(event)
+    CATCHMENT_FILEPATH = "data/catchments_short.json"
+    with open(CATCHMENT_FILEPATH) as f:
+        data = json.load(f)
+    coordinates_raw = []
+    for feature in data["features"]:
+        coordinates_raw.extend(feature["geometry"]["coordinates"])
+    coordinates = []
+    for coord in coordinates_raw:
+        new_coord = Coordinate(lon=coord[0], lat=coord[1])
+        coordinates.append(new_coord)
+    coordinates = list(set(coordinates))
+
+
     now = datetime.utcnow()
     timestamp = now.strftime("%y-%m-%d_%H-%M")
     current_timestamp = str(timestamp)
 
-    MIN_IDX = 0
-    MAX_IDX = 10
     TIMESTAMP = current_timestamp
 
     # Tunable parameters
@@ -38,11 +32,9 @@ def handler(event, context):
     BUCKET_NAME = "all-weather-data"
     AWS_DIR_NAME = "open-meteo"
 
-    coordinates = ALL_COORDINATES[MIN_IDX:MAX_IDX]
     print(f'Uploading current weather data for {len(coordinates)} points')
 
     # Generate timestamp string for directory naming
-
     dir_path = str(TIMESTAMP)
 
     # Instantiate an APIWeatherProvider, AWSDispatcher, and AWSWeatherUploader
