caf1c01f64c07d8ef17abda34325b7303c770cef
orion-junkins <orionjunkins@gmail.com>
2023-05-27 14:42:43 -0700

    Evaluator property accesses changed to func calls in demo

--- demos/demo_forecast_evaluation.py
+++ demos/demo_forecast_evaluation.py
@@ -3,19 +3,19 @@
 from rlf.evaluating.evaluator import build_evaluator_from_csv
 
 # Toggle False if running from the command
-running_as_notebook = False
+running_as_notebook = True
 if running_as_notebook:
     evaluator = build_evaluator_from_csv(path="../data/inference_eval_example.csv")
 else:
     evaluator = build_evaluator_from_csv(path="data/inference_eval_example.csv")
 
 # %% Calculate the mean absolute error for each window size.
-mae = evaluator.df_mae
+mae = evaluator.df_mae()
 print(mae)
 mae.plot()
 
 # %% Calculate the mean absolute percentage error for each window size.
-mape = evaluator.df_mape
+mape = evaluator.df_mape()
 print(mape)
 mape.plot()
 
